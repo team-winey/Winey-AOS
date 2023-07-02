6113206842c4aab53db0a95b7d022b1a1b27058c
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-03 00:55:00 +0900

    [feat] 유용한 확장함수 추가

--- app/src/main/java/com/android/go/sopt/winey/ui/MainActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.go.sopt.winey
+package com.android.go.sopt.winey.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.go.sopt.winey.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
