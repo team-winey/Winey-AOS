8db08819e9dc00dd6120af471f6281a0fc3ddbf6
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-05 12:11:14 +0900

    [chore] #1 중복되는 의존성 제거

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -18,15 +18,15 @@ object AndroidXDependencies {
     const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragmentKtxVersion}"
     const val legacy = "androidx.legacy:legacy-support-v4:${Versions.legacySupportVersion}"
     const val security = "androidx.security:security-crypto:${Versions.securityVersion}"
-    const val lifeCycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleVersion}"
-    const val lifeCycleLiveDataKtx =
+    const val lifecycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleVersion}"
+    const val lifecycleLiveDataKtx =
         "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
+    const val lifecycleViewModelKtx =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
     const val lifecycleJava8 =
         "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycleVersion}"
     const val splashScreen = "androidx.core:core-splashscreen:${Versions.splashVersion}"
     const val pagingRuntime = "androidx.paging:paging-runtime:${Versions.pagingVersion}"
-    const val coroutines =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesAndroidVersion}"
     const val workManager = "androidx.work:work-runtime-ktx:${Versions.workManagerVersion}"
     const val hiltWorkManager = "androidx.hilt:hilt-work:1.0.0"
 }
@@ -43,34 +43,35 @@ object MaterialDesignDependencies {
 }
 
 object KaptDependencies {
-    const val hiltAndroidCompiler = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
     const val hiltCompiler = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
     const val hiltWorkManagerCompiler = "androidx.hilt:hilt-compiler:1.0.0"
 }
 
 object ThirdPartyDependencies {
     const val coil = "io.coil-kt:coil:${Versions.coilVersion}"
-    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
-    const val retrofitJsonConverter =
-        "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Versions.kotlinSerializationConverterVersion}"
+
     const val okHttpBom = "com.squareup.okhttp3:okhttp-bom:${Versions.okHttpVersion}"
     const val okHttp = "com.squareup.okhttp3:okhttp"
     const val okHttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
+    const val retrofitJsonConverter =
+        "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Versions.kotlinSerializationConverterVersion}"
+
     const val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
+    const val ossLicense =
+        "com.google.android.gms:play-services-oss-licenses:${Versions.ossVersion}"
+    const val progressView = "com.github.skydoves:progressview:${Versions.progressViewVersion}"
+    const val balloon = "com.github.skydoves:balloon:${Versions.balloonVersion}"
+    const val lottie = "com.airbnb.android:lottie:${Versions.lottieVersion}"
+
     const val flipper = "com.facebook.flipper:flipper:${Versions.flipperVersion}"
-    const val soloader = "com.facebook.soloader:soloader:${Versions.soloaderVersion}"
     const val flipperNetwork =
         "com.facebook.flipper:flipper-network-plugin:${Versions.flipperVersion}"
     const val flipperLeakCanary =
         "com.facebook.flipper:flipper-leakcanary2-plugin:${Versions.flipperVersion}"
     const val leakCanary =
         "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanaryVersion}"
-    const val ossLicense =
-        "com.google.android.gms:play-services-oss-licenses:${Versions.ossVersion}"
-    const val hiltCore = "com.google.dagger:hilt-core:${Versions.hiltVersion}"
-    const val progressView = "com.github.skydoves:progressview:${Versions.progressViewVersion}"
-    const val balloon = "com.github.skydoves:balloon:${Versions.balloonVersion}"
-    const val lottie = "com.airbnb.android:lottie:${Versions.lottieVersion}"
+    const val soloader = "com.facebook.soloader:soloader:${Versions.soloaderVersion}"
 }
 
 object ClassPathPlugins {
