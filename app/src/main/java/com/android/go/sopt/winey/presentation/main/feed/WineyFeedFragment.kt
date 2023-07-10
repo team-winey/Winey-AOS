f834be809e4a5e94d3cb9bd9e28d45f7c47e28bf
HyeseonBaek <bhs12087@naver.com>
2023-07-10 21:56:20 +0900

    [feat] #7 rv 어댑터 연결

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedFragment.kt
@@ -9,9 +9,19 @@ import com.android.go.sopt.winey.util.binding.BindingFragment
 
 class WineyFeedFragment : BindingFragment<FragmentWineyFeedBinding>(R.layout.fragment_winey_feed) {
     private val viewModel by viewModels<WineyFeedViewModel>()
-
+    private lateinit var wineyFeedAdapter: WineyFeedAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initAdapter()
+        getFeed()
     }
+    private fun initAdapter(){
+        wineyFeedAdapter = WineyFeedAdapter()
+        binding.rvWineyfeedPost.adapter = wineyFeedAdapter
+    }
+
+    private fun getFeed(){
+        wineyFeedAdapter.submitList(viewModel.dummyFeedList)
+    }
+
 }
