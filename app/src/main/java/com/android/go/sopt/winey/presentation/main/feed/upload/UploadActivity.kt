aafcc06ded833bbb84684f91d030fe2a5bc0f1df
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-10 21:44:44 +0900

    [add] #12 위니 업로드 액티비티 추가

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/UploadActivity.kt
@@ -0,0 +1,12 @@
+package com.android.go.sopt.winey.presentation.main.feed.upload
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.go.sopt.winey.R
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
