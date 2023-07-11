00cdfd579eebf5010d4990fa3f28d822433c2d9f
HyeseonBaek <bhs12087@naver.com>
2023-07-11 17:21:46 +0900

    [fix] #7 concat adapter로 변경하여 스크롤문제해결

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedFragment.kt
@@ -3,6 +3,7 @@ package com.android.go.sopt.winey.presentation.main.feed
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ConcatAdapter
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentWineyFeedBinding
 import com.android.go.sopt.winey.util.binding.BindingFragment
@@ -10,17 +11,20 @@ import com.android.go.sopt.winey.util.binding.BindingFragment
 class WineyFeedFragment : BindingFragment<FragmentWineyFeedBinding>(R.layout.fragment_winey_feed) {
     private val viewModel by viewModels<WineyFeedViewModel>()
     private lateinit var wineyFeedAdapter: WineyFeedAdapter
+    private lateinit var wineyFeedHeaderAdapter: WineyFeedHeaderAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
         getFeed()
     }
-    private fun initAdapter(){
+
+    private fun initAdapter() {
         wineyFeedAdapter = WineyFeedAdapter()
-        binding.rvWineyfeedPost.adapter = wineyFeedAdapter
+        wineyFeedHeaderAdapter = WineyFeedHeaderAdapter()
+        binding.rvWineyfeedPost.adapter = ConcatAdapter(wineyFeedHeaderAdapter, wineyFeedAdapter)
     }
 
-    private fun getFeed(){
+    private fun getFeed() {
         wineyFeedAdapter.submitList(viewModel.dummyFeedList)
     }
 
