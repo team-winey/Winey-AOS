b4d6447ead9159e99af1331f95206d8cb6c2e338
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-11 15:12:11 +0900

    [add] #12 업로드 화면에 필요한 3개의 프래그먼트 생성

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/AmountFragment.kt
@@ -0,0 +1,13 @@
+package com.android.go.sopt.winey.presentation.main.feed.upload
+
+import android.os.Bundle
+import android.view.View
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.FragmentAmountBinding
+import com.android.go.sopt.winey.util.binding.BindingFragment
+
+class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_amount) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/ContentFragment.kt
@@ -0,0 +1,13 @@
+package com.android.go.sopt.winey.presentation.main.feed.upload
+
+import android.os.Bundle
+import android.view.View
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.FragmentContentBinding
+import com.android.go.sopt.winey.util.binding.BindingFragment
+
+class ContentFragment : BindingFragment<FragmentContentBinding>(R.layout.fragment_content) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
