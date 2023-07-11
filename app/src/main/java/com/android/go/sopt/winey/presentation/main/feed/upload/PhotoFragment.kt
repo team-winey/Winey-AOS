12a1e2b422467962294579e6b1eb1dadf2f89c3f
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-12 03:51:48 +0900

    [ui] #12 절약 내용 작성 뷰 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/PhotoFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/PhotoFragment.kt
@@ -9,5 +9,8 @@ import com.android.go.sopt.winey.util.binding.BindingFragment
 class PhotoFragment : BindingFragment<FragmentPhotoBinding>(R.layout.fragment_photo) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // todo: 사진 업로드 완료 후 다음 버튼 누르면, 내용 작성 프래그먼트로 전환
+
     }
 }
