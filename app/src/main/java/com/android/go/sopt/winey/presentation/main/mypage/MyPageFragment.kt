e308f0142ff78b9d117b80ba11fe6325774d4800
Sangwook123 <operawook@catholic.ac.kr>
2023-07-12 17:41:15 +0900

    [chore] #13 마이페이지 ui 수정

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/MyPageFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/MyPageFragment.kt
@@ -1,11 +1,13 @@
 package com.android.go.sopt.winey.presentation.main.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentMyPageBinding
 import com.android.go.sopt.winey.util.binding.BindingFragment
+import com.android.go.sopt.winey.util.view.setOnSingleClickListener
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     private val viewModel by viewModels<MyPageViewModel>()
@@ -17,10 +19,22 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
     }
 
     fun initBtnEvent() {
-        binding.btnMypageTargetEdit.setOnClickListener {
+        binding.btnMypageTargetEdit.setOnSingleClickListener {
             val bottomsheet = TargetAmountBottomSheetFragment()
 
             bottomsheet.show(this.childFragmentManager, bottomsheet.tag)
         }
+
+        binding.btnMypage1on1.setOnClickListener {
+            val dialog = ImpossibleDialogFragment()
+
+            dialog.show(this.childFragmentManager, dialog.tag)
+        }
+
+        binding.btnMypageLevelHelp.setOnClickListener {
+            val intent = Intent(context, MypageHelpActivity::class.java)
+
+                startActivity(intent)
+            }
     }
 }
