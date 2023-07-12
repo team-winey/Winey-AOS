e3899c8f5aed8ffcebbb1a27ac5378a1d2e441ff
HyeseonBaek <bhs12087@naver.com>
2023-07-13 03:29:01 +0900

    [add] #18 MyFeedDialogFragment

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/myfeed/MyFeedDialogFragment.kt
@@ -0,0 +1,32 @@
+package com.android.go.sopt.winey.presentation.main.mypage.myfeed
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.DialogMyfeedAlertDeleteBinding
+import com.android.go.sopt.winey.util.binding.BindingDialogFragment
+
+class MyFeedDialogFragment :
+    BindingDialogFragment<DialogMyfeedAlertDeleteBinding>(R.layout.dialog_myfeed_alert_delete) {
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBtnlickListener()
+    }
+
+    private fun initBtnlickListener() {
+        binding.btnDialogCancel.setOnClickListener {
+            this.dismiss()
+        }
+        binding.btnDialogSetgoal.setOnClickListener {
+            /* 목표 구현완료되면 인터랙션 구현 */
+        }
+    }
+}
