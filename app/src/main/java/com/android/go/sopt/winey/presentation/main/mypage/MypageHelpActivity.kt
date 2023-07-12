bb184ff36733b2a58fa811fe28efdca1ff1cf31d
Sangwook123 <operawook@catholic.ac.kr>
2023-07-12 17:43:23 +0900

    [feat] #13 위니사용법 액티비티 추가 및 버튼이벤트 구현

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/MypageHelpActivity.kt
@@ -0,0 +1,27 @@
+package com.android.go.sopt.winey.presentation.main.mypage
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityMypageHelpBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
+
+class MypageHelpActivity : BindingActivity<ActivityMypageHelpBinding>(R.layout.activity_mypage_help) {
+    private val viewModel by viewModels<MyPageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBtnEvent()
+    }
+
+    fun initBtnEvent() {
+        binding.btnHelpBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnHelpClose.setOnClickListener {
+            finish()
+        }
+    }
+}
