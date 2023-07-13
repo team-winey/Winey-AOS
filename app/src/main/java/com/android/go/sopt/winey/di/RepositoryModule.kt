adcd00b330433d39fbfab8402c2595154f57570f
HyeseonBaek <bhs12087@naver.com>
2023-07-13 17:46:55 +0900

    [chore] #25 di module 파일 추가

--- app/src/main/java/com/android/go/sopt/winey/di/RepositoryModule.kt
+++ app/src/main/java/com/android/go/sopt/winey/di/RepositoryModule.kt
@@ -1,4 +1,15 @@
 package com.android.go.sopt.winey.di
 
-class RepositoryModule {
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    /*
+    @Singleton
+    @Binds
+    abstract fun providesExampleRepo(exampleRepository: ExampleRepositoryImpl): ExampleRepository = exampleRepository
+    */
 }
--- app/src/main/java/com/android/go/sopt/winey/di/RetrofitModule.kt
+++ app/src/main/java/com/android/go/sopt/winey/di/RetrofitModule.kt
@@ -1,4 +1,63 @@
 package com.android.go.sopt.winey.di
 
-class RetrofitModule {
+import com.android.go.sopt.winey.BuildConfig.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideJsonConverter(json: Json): Converter.Factory =
+        json.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): Interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(interceptor: HttpLoggingInterceptor): Interceptor = interceptor
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        factory: Converter.Factory,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(factory)
+        .build()
 }
