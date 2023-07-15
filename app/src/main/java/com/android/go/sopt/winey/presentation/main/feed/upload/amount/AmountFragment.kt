53c4a2c181c385c2c640c6508bba08ae937c7db0
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-15 20:57:34 +0900

    [feat] #16 금액 입력 시 천 단위로 콤마 표시

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/amount/AmountFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/amount/AmountFragment.kt
@@ -1,6 +1,8 @@
 package com.android.go.sopt.winey.presentation.main.feed.upload.amount
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -13,6 +15,8 @@ import com.android.go.sopt.winey.util.binding.BindingFragment
 import com.android.go.sopt.winey.util.context.hideKeyboard
 import com.android.go.sopt.winey.util.fragment.toast
 import com.android.go.sopt.winey.util.view.setOnSingleClickListener
+import java.math.BigInteger
+import java.text.DecimalFormat
 
 class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_amount) {
     private val viewModel by viewModels<AmountViewModel>()
@@ -26,6 +30,7 @@ class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_
         initRootLayoutClickListener()
         initBackButtonClickListener()
         initUploadButtonClickListener()
+        initEditTextWatcher()
     }
 
     private fun initBackButtonClickListener() {
@@ -39,6 +44,31 @@ class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_
         binding.btnAmountNext.setOnSingleClickListener {
             toast("$content ${viewModel.amount}")
         }
+    }
+
+    private fun initEditTextWatcher() {
+        var temp = ""
+        binding.etUploadAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+
+                // 문자열이 비어있거나 이전과 변함 없으면 그대로 둔다.
+                if (input.isBlank() || input == temp) return
+
+                temp = makeCommaNumber(input.replace(",", "").toBigInteger())
+                binding.etUploadAmount.apply {
+                    setText(temp)
+                    setSelection(temp.length) // 커서를 오른쪽 끝으로 보낸다.
+                }
+            }
+        })
+    }
+
+    private fun makeCommaNumber(input: BigInteger): String {
+        val formatter = DecimalFormat("#,###")
+        return formatter.format(input)
     }
 
     private inline fun <reified T : Fragment> navigateTo() {
