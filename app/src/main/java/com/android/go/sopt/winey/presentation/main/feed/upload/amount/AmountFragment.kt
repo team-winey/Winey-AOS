7db5a006b1254516807a90a52956f23e22257f5f
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-15 17:27:47 +0900

    [feat] #16 금액 작성 화면에서 이전 버튼 클릭 리스너 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/amount/AmountFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/amount/AmountFragment.kt
@@ -2,9 +2,13 @@ package com.android.go.sopt.winey.presentation.main.feed.upload.amount
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentAmountBinding
+import com.android.go.sopt.winey.presentation.main.feed.upload.content.ContentFragment
 import com.android.go.sopt.winey.util.binding.BindingFragment
 
 class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_amount) {
@@ -14,6 +18,18 @@ class AmountFragment : BindingFragment<FragmentAmountBinding>(R.layout.fragment_
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
 
+        initBackButtonClickListener()
+    }
 
+    private fun initBackButtonClickListener() {
+        binding.ivAmountBack.setOnClickListener {
+            navigateTo<ContentFragment>()
+        }
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        requireActivity().supportFragmentManager.commit {
+            replace<T>(R.id.fcv_upload, T::class.simpleName)
+        }
     }
 }
