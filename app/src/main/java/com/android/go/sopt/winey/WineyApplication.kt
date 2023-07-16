f89b10c653e2d4edd0554d84d174c8a32933e6a1
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-16 10:46:40 +0900

    [add] #1 Timber 추가

--- app/src/main/java/com/android/go/sopt/winey/WineyApplication.kt
+++ app/src/main/java/com/android/go/sopt/winey/WineyApplication.kt
@@ -1,11 +1,13 @@
 package com.android.go.sopt.winey
 
-import dagger.hilt.android.HiltAndroidApp
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class WineyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
