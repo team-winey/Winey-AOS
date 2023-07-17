9886e59facd14f0f3e49d831f3964db189bd377d
HyeseonBaek <bhs12087@naver.com>
2023-07-17 22:44:16 +0900

    [mod] #28 목표설정 클릭시 마이페이지로 전환되도록

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedDialogFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedDialogFragment.kt
@@ -1,34 +1,35 @@
 package com.android.go.sopt.winey.presentation.main.feed
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.commit
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentWineyfeedNoGoalDialogBinding
-import com.android.go.sopt.winey.presentation.main.mypage.TargetAmountBottomSheetFragment
+import com.android.go.sopt.winey.presentation.main.mypage.MyPageFragment
 import com.android.go.sopt.winey.util.binding.BindingDialogFragment
 
 class WineyFeedDialogFragment :
     BindingDialogFragment<FragmentWineyfeedNoGoalDialogBinding>(R.layout.fragment_wineyfeed_no_goal_dialog) {
 
-    override fun onStart() {
-        super.onStart()
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initBtnlickListener()
+        initBtnClickListener()
     }
 
-    private fun initBtnlickListener() {
+    private fun initBtnClickListener() {
         binding.btnDialogCancel.setOnClickListener {
             this.dismiss()
         }
         binding.btnDialogSetgoal.setOnClickListener {
-            val bottomSheet = TargetAmountBottomSheetFragment()
-            bottomSheet.show(this.childFragmentManager, bottomSheet.tag)
+            this.dismiss()
+            navigateToMyPage()
+        }
+    }
+
+    private fun navigateToMyPage() {
+        parentFragmentManager.commit {
+            replace(R.id.fcv_main, MyPageFragment())
+            addToBackStack(null)
         }
     }
 }
