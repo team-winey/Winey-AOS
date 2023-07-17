e7d70cf01611c0f5c3e5f03ebd7b41b42f260497
HyeseonBaek <bhs12087@naver.com>
2023-07-17 15:37:19 +0900

    [feat] #29 에러 코드별 예외처리

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedViewModel.kt
@@ -1,6 +1,5 @@
 package com.android.go.sopt.winey.presentation.main.feed
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,6 +9,8 @@ import com.android.go.sopt.winey.domain.repository.AuthRepository
 import com.android.go.sopt.winey.util.view.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -36,12 +37,25 @@ class WineyFeedViewModel @Inject constructor(
                     _getListState.value = UiState.Success(response)
                     _WineyFeedListLiveData.value = response
                 }
-                .onFailure {
-                    _getListState.value = UiState.Failure(WINEYFEED_FAIL_MSG)
+                .onFailure { t ->
+                    if (t is HttpException) {
+                        when (t.code()) {
+                            CODE_WINEYFEED_INVALID_USER -> _getListState.value =
+                                UiState.Failure(CODE_WINEYFEED_INVALID_USER.toString())
+
+                            CODE_WINEYFEED_INVALID_REQUEST -> _getListState.value =
+                                    UiState.Failure(CODE_WINEYFEED_INVALID_USER.toString())
+                            else -> _getListState.value = UiState.Failure(MSG_WINEYFEED_FAIL)
+                        }
+                        Timber.e("$MSG_WINEYFEED_FAIL : ${t.code()} : ${t.message()}")
+                    }
                 }
         }
     }
+
     companion object {
-        private const val WINEYFEED_FAIL_MSG = "getWineyFeed fail"
+        private const val CODE_WINEYFEED_INVALID_USER = 404
+        private const val CODE_WINEYFEED_INVALID_REQUEST = 400
+        private const val MSG_WINEYFEED_FAIL = "FAIL"
     }
 }
