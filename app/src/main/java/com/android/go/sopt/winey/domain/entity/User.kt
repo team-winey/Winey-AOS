1375c06e7ab9ac9c2db52cd7b7288343448c5909
Sangwook123 <operawook@catholic.ac.kr>
2023-07-17 20:07:21 +0900

    [chore] #24 클래스명 변경

---
+++ app/src/main/java/com/android/go/sopt/winey/domain/entity/User.kt
@@ -0,0 +1,12 @@
+package com.android.go.sopt.winey.domain.entity
+
+data class User(
+    val nickname: String,
+    val userLevel: String,
+    val duringGoalAmount: Int,
+    val duringGoalCount: Int,
+    val targetMoney: Int,
+    val targetDay: Int,
+    val isOver: Boolean,
+    val isAttained: Boolean
+)
