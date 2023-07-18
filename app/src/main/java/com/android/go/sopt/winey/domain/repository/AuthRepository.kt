2a43b52c57adb02d361eceb36a438f09fe70a5d5
HyeseonBaek <bhs12087@naver.com>
2023-07-18 17:44:22 +0900

    [add] #39 피드 좋아요, 취소 repository 생성

--- app/src/main/java/com/android/go/sopt/winey/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/android/go/sopt/winey/domain/repository/AuthRepository.kt
@@ -1,9 +1,12 @@
 package com.android.go.sopt.winey.domain.repository
 
+import com.android.go.sopt.winey.data.model.remote.request.RequestPostLikeDto
+import com.android.go.sopt.winey.domain.entity.Like
 import com.android.go.sopt.winey.domain.entity.User
 import com.android.go.sopt.winey.domain.entity.WineyFeed
 
 interface AuthRepository {
     suspend fun getUser(): Result<User>
     suspend fun getWineyFeed(page: Int): Result<List<WineyFeed>>
+    suspend fun postFeedLike(feedId: Int, requestPostLikeDto: RequestPostLikeDto): Result<Like>
 }
