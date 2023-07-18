54a1ed9dd4fdea36f2a54e3974ecfb4c26db4cea
Sangwook123 <operawook@catholic.ac.kr>
2023-07-18 21:08:58 +0900

    [feat] #41 textinputlayout 에러메시지 출력 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/TargetAmountBottomSheetFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/TargetAmountBottomSheetFragment.kt
@@ -14,18 +14,23 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class TargetAmountBottomSheetFragment :
     BindingBottomSheetDialogFragment<FragmentTargetAmountBottomSheetBinding>(R.layout.fragment_target_amount_bottom_sheet) {
-    private val viewModel by viewModels<MyPageViewModel>()
+    private val viewModel by viewModels<TargetAmountViewModel>()
 
     override fun onStart() {
         super.onStart()
 
         initScreenHeight()
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.data = viewModel
         initCancelButtonClickListener()
+        initAmountEditTextWatcher()
+        initDayEditTextWatcher()
+        initAmountCheckObserver()
+        initDayCheckObserver()
     }
 
 
@@ -35,6 +40,141 @@ class TargetAmountBottomSheetFragment :
         }
     }
 
+    private fun initAmountEditTextWatcher() {
+        var temp = ""
+        binding.etTargetAmountSetAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val input = s.toString()
+                val amount: String = if (input.isNotBlank()) {
+                    input.replace(",", "")
+                } else {
+                    "0"
+                }
+                viewModel.checkAmount(amount)
+                viewModel.checkButtonState()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+
+                // 문자열이 비어있거나 이전과 변함 없으면 그대로 둔다.
+                if (input.isBlank() || input == temp) return
+
+                temp = makeCommaString(input.replace(",", "").toLong())
+                binding.etTargetAmountSetAmount.apply {
+                    setText(temp)
+                    setSelection(temp.length) // 커서를 오른쪽 끝으로 보낸다.
+                }
+            }
+        })
+    }
+
+    private fun initDayEditTextWatcher() {
+        var temp = ""
+        binding.etTargetAmountSetDay.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val input = s.toString()
+                val day: String = if (input.isNotBlank()) {
+                    input.replace(",", "")
+                } else {
+                    "0"
+                }
+                viewModel.checkDay(day)
+                viewModel.checkButtonState()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+
+                // 문자열이 비어있거나 이전과 변함 없으면 그대로 둔다.
+                if (input.isBlank() || input == temp) return
+
+                temp = makeCommaString(input.replace(",", "").toLong())
+                binding.etTargetAmountSetDay.apply {
+                    setText(temp)
+                    setSelection(temp.length) // 커서를 오른쪽 끝으로 보낸다.
+                }
+            }
+        })
+    }
+
+    private fun initAmountCheckObserver() {
+        viewModel.amountCheck.observe(viewLifecycleOwner) {
+            when (it) {
+                true -> {
+                    binding.tilTargetAmountSetAmount.error = " "
+                    binding.etTargetAmountSetAmount.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.red_500
+                        )
+                    )
+                    binding.tvTargetAmountWarningAmount.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.red_500
+                        )
+                    )
+                }
+
+                false -> {
+                    binding.tilTargetAmountSetAmount.error = null
+                    binding.etTargetAmountSetAmount.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.purple_400
+                        )
+                    )
+                    binding.tvTargetAmountWarningAmount.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.gray_400
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun initDayCheckObserver() {
+        viewModel.dayCheck.observe(viewLifecycleOwner) {
+            when (it) {
+                true -> {
+                    binding.tilTargetAmountSetDay.error = " "
+                    binding.etTargetAmountSetDay.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.red_500
+                        )
+                    )
+                    binding.tvTargetAmountWarningDay.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.red_500
+                        )
+                    )
+                }
+
+                false -> {
+                    binding.tilTargetAmountSetDay.error = null
+                    binding.etTargetAmountSetDay.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.purple_400
+                        )
+                    )
+                    binding.tvTargetAmountWarningDay.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.gray_400
+                        )
+                    )
+                }
+            }
+        }
+    }
     fun initScreenHeight() {
         if (dialog != null) {
             val bottomSheet: View =
--- app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/TargetAmountViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/TargetAmountViewModel.kt
@@ -9,4 +9,25 @@ class TargetAmountViewModel : ViewModel() {
     val amount: LiveData<String> = _amount
     val _day = MutableLiveData<String>()
     val day: LiveData<String> = _day
+
+    private val _amountCheck = MutableLiveData<Boolean>()
+    val amountCheck: LiveData<Boolean> = _amountCheck
+
+    private val _dayCheck = MutableLiveData<Boolean>()
+    val dayCheck: LiveData<Boolean> = _dayCheck
+    fun checkDay(Day: String) {
+        if (Day.toLong() > 0 && Day.toLong() < 5 && !Day.isNullOrEmpty()) {
+            _dayCheck.value = true
+        } else {
+            _dayCheck.value = false
+        }
+    }
+
+    fun checkAmount(Amount: String) {
+        if (Amount.toLong() > 0 && Amount.toLong() < 30000 && !Amount.isNullOrEmpty()) {
+            _amountCheck.value = true
+        } else {
+            _amountCheck.value = false
+        }
+    }
 }
