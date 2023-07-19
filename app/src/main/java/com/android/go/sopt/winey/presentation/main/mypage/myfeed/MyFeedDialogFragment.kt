8a48dc6e1326f48e491032247859b261b4f60d6b
HyeseonBaek <bhs12087@naver.com>
2023-07-19 17:50:07 +0900

    [feat] #49 rv에 피드 삭제하기 상태 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/myfeed/MyFeedDialogFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/myfeed/MyFeedDialogFragment.kt
@@ -1,32 +1,68 @@
 package com.android.go.sopt.winey.presentation.main.mypage.myfeed
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentMyfeedDeleteDialogBinding
 import com.android.go.sopt.winey.util.binding.BindingDialogFragment
+import com.android.go.sopt.winey.util.fragment.snackBar
+import com.android.go.sopt.winey.util.view.UiState
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
-class MyFeedDialogFragment :
+@AndroidEntryPoint
+class MyFeedDialogFragment(private val feedId: Int) :
     BindingDialogFragment<FragmentMyfeedDeleteDialogBinding>(R.layout.fragment_myfeed_delete_dialog) {
-
-    override fun onStart() {
-        super.onStart()
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-    }
+    lateinit var myFeedFragment: MyFeedFragment
+    private val myFeedViewModel by viewModels<MyFeedViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initBtnlickListener()
+        initButtonClickListener()
+        initDeleteFeedStateObserver()
+
     }
 
-    private fun initBtnlickListener() {
+    private fun initButtonClickListener() {
         binding.btnDialogCancel.setOnClickListener {
             this.dismiss()
         }
         binding.btnDialogDelete.setOnClickListener {
-            /* 마이위니 구현완료되면 인터랙션 구현 */
+            myFeedViewModel.deleteFeed(feedId)
+            initDeleteFeedStateObserver()
         }
+    }
+
+    private fun initDeleteFeedStateObserver() {
+        myFeedFragment = MyFeedFragment()
+        myFeedViewModel.deleteMyFeedState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    this.dismiss()
+                    /* 그리고 viewmodel 각각 만들었는데 onPause될때 viewmodel detach되므로
+                    공유하려고 같이쓰는형태*/
+                    /* 재생성ver */
+                    val fragmentManager = requireActivity().supportFragmentManager
+                    fragmentManager.beginTransaction().apply {
+                        myFeedFragment = MyFeedFragment()
+                        replace(R.id.fcv_main, myFeedFragment)
+                        commit()
+                    }
+                    snackBar(binding.root) { state.toString() }
+                }
+
+                is UiState.Failure -> {
+                    snackBar(binding.root) { state.msg }
+                }
+
+                else -> Timber.tag("failure").e(MSG_MYFEED_ERROR)
+            }
+        }
+    }
+
+
+    companion object {
+        private const val MSG_MYFEED_ERROR = "ERROR"
     }
 }
---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/mypage/myfeed/MyFeedDialogViewModel.kt
@@ -0,0 +1,57 @@
+package com.android.go.sopt.winey.presentation.main.mypage.myfeed
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.go.sopt.winey.domain.repository.AuthRepository
+import com.android.go.sopt.winey.util.view.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class MyFeedDialogViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    val _deleteMyFeedState = MutableLiveData<UiState<Unit>>(UiState.Loading)
+    val deleteMyFeedState: LiveData<UiState<Unit>>
+        get() = _deleteMyFeedState
+
+    fun deleteFeed(feedId: Int) {
+        viewModelScope.launch {
+            authRepository.deleteFeed(feedId)
+                .onSuccess { state ->
+                    _deleteMyFeedState.value = UiState.Success(state)
+                    Log.e("deleteSuccess", state.toString())
+                }
+                .onFailure { t ->
+                    if (t is HttpException) {
+                        Log.e("deleteFail", t.message())
+                        _deleteMyFeedState.value.apply {
+                            when (t.code()) {
+                                CODE_MYFEED_INVALID_USER ->
+                                    UiState.Failure(t.message())
+
+                                CODE_MYFEED_INVALID_REQUEST ->
+                                    UiState.Failure(t.message())
+
+                                else -> _deleteMyFeedState.value =
+                                    UiState.Failure(t.message())
+                            }
+                        }
+                        Timber.e("${MSG_MYFEED_FAIL} : ${t.code()} : ${t.message()}")
+                    }
+                }
+        }
+    }
+
+    companion object {
+        private const val CODE_MYFEED_INVALID_USER = 404
+        private const val CODE_MYFEED_INVALID_REQUEST = 400
+        private const val MSG_MYFEED_FAIL = "FAIL"
+    }
+}
