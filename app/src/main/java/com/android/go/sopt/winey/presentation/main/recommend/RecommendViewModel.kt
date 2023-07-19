7d4771b631783991ad98c9a50b77d12e00e865c7
Sangwook123 <operawook@catholic.ac.kr>
2023-07-19 22:52:25 +0900

    [feat] #58 추천위니 서버통신 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/recommend/RecommendViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/recommend/RecommendViewModel.kt
@@ -1,9 +1,22 @@
 package com.android.go.sopt.winey.presentation.main.recommend
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.android.go.sopt.winey.domain.entity.Recommend
+import com.android.go.sopt.winey.domain.repository.AuthRepository
+import com.android.go.sopt.winey.util.view.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
 
-class RecommendViewModel : ViewModel() {
+@HiltViewModel
+class RecommendViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+): ViewModel() {
     val recommendList = listOf(
         Recommend(
             id = 1,
@@ -54,4 +67,28 @@ class RecommendViewModel : ViewModel() {
             image = "asdasdasd",
         ),
     )
+
+    init {
+        getRecommendList()
+    }
+
+    private val _getRecommendListState = MutableLiveData<UiState<List<Recommend>>>(UiState.Loading)
+    val getRecommendListState : LiveData<UiState<List<Recommend>>> = _getRecommendListState
+
+    fun getRecommendList(){
+        viewModelScope.launch {
+            authRepository.getRecommendList(1)
+                .onSuccess { response ->
+                    _getRecommendListState.value = UiState.Success(response)
+                    Log.e("test log", "성공")
+                }
+                .onFailure { t ->
+                    if (t is HttpException) {
+                        Log.e("test log", "HTTP 실패")
+                    }
+                    Log.e("test log", "${t.message}")
+                    _getRecommendListState.value = UiState.Failure("${t.message}")
+                }
+        }
+    }
 }
