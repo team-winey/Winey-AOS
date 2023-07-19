df20430078a960d3b6b397571c9cbc1bf14a4333
leeeha <jxlhe46@seoultech.ac.kr>
2023-07-19 18:41:56 +0900

    [feat] #42 3초 지나면 로딩 액티비티 종료

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/loading/LoadingActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/loading/LoadingActivity.kt
@@ -1,6 +1,7 @@
 package com.android.go.sopt.winey.presentation.main.feed.upload.loading
 
 import android.os.Bundle
+import android.os.Handler
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityLoadingBinding
 import com.android.go.sopt.winey.util.binding.BindingActivity
@@ -9,5 +10,12 @@ class LoadingActivity : BindingActivity<ActivityLoadingBinding>(R.layout.activit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Handler().postDelayed({
+            finish()
+        }, DELAY_TIME)
+    }
+
+    companion object {
+        private const val DELAY_TIME = 3000L
     }
 }
