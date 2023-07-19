851132784aa33310dbf5fb4bd5c2e2122e107389
Sangwook123 <operawook@catholic.ac.kr>
2023-07-19 16:56:14 +0900

    [feat] #55 추천위니 헤더 어댑터 구현

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/recommend/RecommendHeaderAdapter.kt
@@ -0,0 +1,29 @@
+package com.android.go.sopt.winey.presentation.main.feed
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.go.sopt.winey.databinding.ItemRecommendHeaderBinding
+
+class RecommendHeaderAdapter : RecyclerView.Adapter<RecommendHeaderAdapter.HeaderViewHolder>() {
+
+    class HeaderViewHolder(
+        private val binding: ItemRecommendHeaderBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
+        val binding =
+            ItemRecommendHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HeaderViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HeaderViewHolder, position: Int) {
+        return
+    }
+
+    override fun getItemCount(): Int = HEADER_COUNT
+
+    companion object {
+        private const val HEADER_COUNT = 1
+    }
+}
