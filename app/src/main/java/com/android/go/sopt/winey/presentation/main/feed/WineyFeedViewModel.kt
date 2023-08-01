785d4b7b9b7ce3c7c0d6f450aefdfde1d8aaf183
HyeseonBaek <bhs12087@naver.com>
2023-08-01 17:33:46 +0900

    [refractor] #90 제네릭 함수 추가해 중복 failure 처리 코드 제거

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/WineyFeedViewModel.kt
@@ -53,23 +53,7 @@ class WineyFeedViewModel @Inject constructor(
                 .onSuccess { state ->
                     _deleteMyFeedState.value = UiState.Success(state)
                 }
-                .onFailure { t ->
-                    if (t is HttpException) {
-                        _deleteMyFeedState.value.apply {
-                            when (t.code()) {
-                                CODE_WINEYFEED_INVALID_USER ->
-                                    UiState.Failure(t.message())
-
-                                CODE_WINEYFEED_INVALID_REQUEST ->
-                                    UiState.Failure(t.message())
-
-                                else -> _deleteMyFeedState.value =
-                                    UiState.Failure(t.message())
-                            }
-                        }
-                        Timber.e("$MSG_WINEYFEED_FAIL} : ${t.code()} : ${t.message()}")
-                    }
-                }
+                .onFailure { t -> handleFailureState(_deleteMyFeedState, t) }
         }
     }
 
@@ -79,22 +63,7 @@ class WineyFeedViewModel @Inject constructor(
                 .onSuccess { state ->
                     _postWineyFeedLikeState.value = UiState.Success(state)
                 }
-                .onFailure { t ->
-                    if (t is HttpException) {
-                        _postWineyFeedLikeState.value.apply {
-                            when (t.code()) {
-                                CODE_WINEYFEED_INVALID_USER ->
-                                    UiState.Failure(t.message())
-
-                                CODE_WINEYFEED_INVALID_REQUEST ->
-                                    UiState.Failure(t.message())
-
-                                else -> UiState.Failure(t.message())
-                            }
-                        }
-                        Timber.e("$MSG_WINEYFEED_FAIL : ${t.code()} : ${t.message()}")
-                    }
-                }
+                .onFailure { t -> handleFailureState(_postWineyFeedLikeState, t) }
         }
     }
 
@@ -116,23 +85,20 @@ class WineyFeedViewModel @Inject constructor(
                         val updatedList = currentMutableList.toList()
                         _getWineyFeedListState.value = UiState.Success(updatedList)
                     }
-                    .onFailure { t ->
-                        if (t is HttpException) {
-                            _getWineyFeedListState.value.apply {
-                                when (t.code()) {
-                                    CODE_WINEYFEED_INVALID_USER ->
-                                        UiState.Failure(t.message())
-
-                                    CODE_WINEYFEED_INVALID_REQUEST ->
-                                        UiState.Failure(t.message())
-
-                                    else -> UiState.Failure(t.message())
-                                }
-                            }
-                            Timber.e("$MSG_WINEYFEED_FAIL : ${t.code()} : ${t.message()}")
-                        }
-                    }
+                    .onFailure { t -> handleFailureState(_postWineyFeedLikeState, t) }
             }
+        }
+    }
+
+    private fun <T> handleFailureState(loadingState: MutableLiveData<UiState<T>>, t: Throwable) {
+        if (t is HttpException) {
+            val errorMessage = when (t.code()) {
+                CODE_WINEYFEED_INVALID_USER -> t.message()
+                CODE_WINEYFEED_INVALID_REQUEST -> t.message()
+                else -> t.message()
+            }
+            loadingState.value = UiState.Failure(errorMessage)
+            Timber.e("$MSG_WINEYFEED_FAIL : ${t.code()} : ${t.message()}")
         }
     }
 
