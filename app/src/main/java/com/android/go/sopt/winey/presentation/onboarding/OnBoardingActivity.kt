cd762c6664252d2931c833af97d68edcdf17323b
Sangwook123 <operawook@catholic.ac.kr>
2023-08-02 16:44:17 +0900

    [feat] #97 카카오 로그인 구현

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,46 @@
+package com.android.go.sopt.winey.presentation.onboarding
+
+import android.os.Bundle
+import android.util.Log
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityOnBoardingBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+
+class OnBoardingActivity: BindingActivity<ActivityOnBoardingBinding>(R.layout.activity_on_boarding) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.btnOnboardingKakao.setOnClickListener {
+
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("LOGIN", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
+
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i("LOGIN", "카카오톡으로 로그인 성공 ${token.accessToken}")
+
+                        finish()
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+    }
+}
--- app/src/main/java/com/android/go/sopt/winey/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/splash/SplashActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.presentation.main.MainActivity
+import com.android.go.sopt.winey.presentation.onboarding.OnBoardingActivity
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,14 +17,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         Handler(Looper.getMainLooper()).postDelayed({
             navigateToMainScreen()
         }, DELAY_TIME)
     }
 
     private fun navigateToMainScreen() {
-        Intent(this@SplashActivity, MainActivity::class.java).apply {
+        Intent(this@SplashActivity, OnBoardingActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(this)
         }
