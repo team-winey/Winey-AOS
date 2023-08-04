3b26e0ad5e4911a8cbc884f3376a590f4899263e
Sangwook123 <operawook@catholic.ac.kr>
2023-08-05 06:36:29 +0900

    [feat] #97 기존코드 삭제, 버튼클릭 함수화, 뷰모델 함수사용

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/OnBoardingActivity.kt
@@ -1,48 +1,25 @@
 package com.android.go.sopt.winey.presentation.onboarding
 
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityOnBoardingBinding
 import com.android.go.sopt.winey.util.binding.BindingActivity
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.AndroidEntryPoint
 
-class OnBoardingActivity: BindingActivity<ActivityOnBoardingBinding>(R.layout.activity_on_boarding) {
+@AndroidEntryPoint
+class OnBoardingActivity :
+    BindingActivity<ActivityOnBoardingBinding>(R.layout.activity_on_boarding) {
+    val viewModel: OnBoardingViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initKakaoLoginButtonClickListener()
+    }
+
+    private fun initKakaoLoginButtonClickListener(){
         binding.btnOnboardingKakao.setOnClickListener {
-
-            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-                if (error != null) {
-                    Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
-                } else if (token != null) {
-                    Log.i("LOGIN", "카카오계정으로 로그인 성공 액세스토큰 ${token.accessToken}")
-                    Log.i("LOGIN", "카카오계정으로 로그인 성공 리프레시토큰 ${token.refreshToken}")
-                }
-            }
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                    if (error != null) {
-                        Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
-
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            return@loginWithKakaoTalk
-                        }
-                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-                    } else if (token != null) {
-                        Log.i("LOGIN", "카카오톡으로 로그인 성공 액세스토큰 ${token.accessToken}")
-                        Log.i("LOGIN", "카카오톡으로 로그인 성공 리프레시토큰 ${token.refreshToken}")
-
-                        finish()
-                    }
-                }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-            }
+            viewModel.kakaoLogin()
         }
     }
 }
