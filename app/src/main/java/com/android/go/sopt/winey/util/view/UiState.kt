49b6e3380c19d41b877289a99ebd4a9e1881bc13
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-06 00:27:40 +0900

    [del] 사용하지 않는 코드 삭제

--- app/src/main/java/com/android/go/sopt/winey/util/view/UiState.kt
+++ app/src/main/java/com/android/go/sopt/winey/util/view/UiState.kt
@@ -12,20 +12,4 @@ sealed interface UiState<out T> {
     data class Failure(
         val msg: String
     ) : UiState<Nothing>
-
-    fun getUiStateModel(): UiStateModel {
-        return UiStateModel(
-            this is Empty,
-            this is Loading,
-            this is Success,
-            this is Failure
-        )
-    }
 }
-
-data class UiStateModel(
-    val isEmpty: Boolean = false,
-    val isLoading: Boolean = true,
-    val isSuccess: Boolean = false,
-    val isFailure: Boolean = false
-)
