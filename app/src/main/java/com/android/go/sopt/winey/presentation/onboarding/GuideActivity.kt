e7a1c08f9eca591981fabec98f4b6f71f5eb70d5
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-06 00:46:24 +0900

    [add] #95 온보딩 가이드 액티비티 추가

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/GuideActivity.kt
@@ -0,0 +1,12 @@
+package com.android.go.sopt.winey.presentation.onboarding
+
+import android.os.Bundle
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityGuideBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
+
+class GuideActivity : BindingActivity<ActivityGuideBinding>(R.layout.activity_guide) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
