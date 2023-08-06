eeab1503651059af411bfa4aba25106c2180f1ea
Sangwook123 <operawook@catholic.ac.kr>
2023-08-07 01:52:21 +0900

    [feat] #97 인터셉터에서 저장된 토큰 사용

--- app/src/main/java/com/android/go/sopt/winey/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/android/go/sopt/winey/data/interceptor/AuthInterceptor.kt
@@ -1,22 +1,33 @@
 package com.android.go.sopt.winey.data.interceptor
 
 import android.content.Context
+import com.android.go.sopt.winey.domain.repository.DataStoreRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
-    @ApplicationContext context: Context
+    @ApplicationContext context: Context,
+    private val dataStoreRepository: DataStoreRepository
 ) : Interceptor {
+
     override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = runBlocking { getAccessToken() }
+
         val originalRequest = chain.request()
 
         val headerRequest = originalRequest.newBuilder()
-            .addHeader(HEADER_TOKEN, USER_ID)
+            .addHeader(HEADER_TOKEN, accessToken)
             .build()
 
         return chain.proceed(headerRequest)
+    }
+
+    private suspend fun getAccessToken(): String {
+        return dataStoreRepository.getAccessToken().first() ?: ""
     }
 
     companion object {
