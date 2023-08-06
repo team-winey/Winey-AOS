f1fc944de4780a59b09acb3815d313aaa310054f
Sangwook123 <operawook@catholic.ac.kr>
2023-08-07 01:47:04 +0900

    [feat] #97 postLogin 함수 datasource에 작성

--- app/src/main/java/com/android/go/sopt/winey/data/source/AuthDataSource.kt
+++ app/src/main/java/com/android/go/sopt/winey/data/source/AuthDataSource.kt
@@ -1,11 +1,13 @@
 package com.android.go.sopt.winey.data.source
 
 import com.android.go.sopt.winey.data.model.remote.request.RequestCreateGoalDto
+import com.android.go.sopt.winey.data.model.remote.request.RequestLoginDto
 import com.android.go.sopt.winey.data.model.remote.request.RequestPostLikeDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponseCreateGoalDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponseGetRecommendListDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponseGetUserDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponseGetWineyFeedListDto
+import com.android.go.sopt.winey.data.model.remote.response.ResponseLoginDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponsePostLikeDto
 import com.android.go.sopt.winey.data.model.remote.response.ResponsePostWineyFeedDto
 import com.android.go.sopt.winey.data.model.remote.response.base.BaseResponse
@@ -25,7 +27,10 @@ class AuthDataSource @Inject constructor(
     suspend fun getMyFeedList(page: Int): ResponseGetWineyFeedListDto =
         authService.getMyFeedList(page)
 
-    suspend fun postFeedLike(feedId: Int, requestPostLikeDto: RequestPostLikeDto): ResponsePostLikeDto =
+    suspend fun postFeedLike(
+        feedId: Int,
+        requestPostLikeDto: RequestPostLikeDto
+    ): ResponsePostLikeDto =
         authService.postFeedLike(feedId, requestPostLikeDto)
 
     suspend fun postWineyFeedList(
@@ -39,6 +44,13 @@ class AuthDataSource @Inject constructor(
 
     suspend fun getRecommendList(page: Int): BaseResponse<ResponseGetRecommendListDto> =
         authService.getRecommendList(page)
+
     suspend fun deleteFeed(feedId: Int): BaseResponse<Unit> =
         authService.deleteFeed(feedId)
+
+    suspend fun postLogin(
+        accessToken: String,
+        requestLoginDto: RequestLoginDto
+    ): BaseResponse<ResponseLoginDto> =
+        authService.postLogin(accessToken, requestLoginDto)
 }
