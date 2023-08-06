3819c23de9b5b5ee9bd31b1731074eb475de08da
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-06 19:16:52 +0900

    [ui] #101 닉네임 액티비티 뷰 구현하는 중

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/nickname/NicknameActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/nickname/NicknameActivity.kt
@@ -1,12 +1,12 @@
 package com.android.go.sopt.winey.presentation.onboarding.nickname
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityNicknameBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
 
-class NicknameActivity : AppCompatActivity() {
+class NicknameActivity : BindingActivity<ActivityNicknameBinding>(R.layout.activity_nickname) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nickname)
     }
 }
