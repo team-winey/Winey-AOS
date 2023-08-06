58635f5aae9c2d66d03429001e00df18e8cc7251
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-06 18:01:49 +0900

    [add] #101 닉네임 액티비티 추가

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/nickname/NicknameActivity.kt
@@ -0,0 +1,12 @@
+package com.android.go.sopt.winey.presentation.onboarding.nickname
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.go.sopt.winey.R
+
+class NicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nickname)
+    }
+}
