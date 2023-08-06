9909c297d448b9013ca67057e5073e88ea5738e4
Sangwook123 <operawook@catholic.ac.kr>
2023-08-07 05:43:38 +0900

    [feat] #97 자동로그인 구현

--- app/src/main/java/com/android/go/sopt/winey/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/splash/SplashActivity.kt
@@ -4,26 +4,47 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.domain.repository.DataStoreRepository
 import com.android.go.sopt.winey.presentation.main.MainActivity
 import com.android.go.sopt.winey.presentation.onboarding.OnBoardingActivity
-import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    @Inject
+    lateinit var dataStoreRepository: DataStoreRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
-            navigateToMainScreen()
+            checkAccessTokenAndNavigate()
         }, DELAY_TIME)
     }
 
-    private fun navigateToMainScreen() {
+    private fun checkAccessTokenAndNavigate() {
+        val accessToken = runBlocking { dataStoreRepository.getAccessToken().firstOrNull() }
+        if (accessToken.isNullOrEmpty() || accessToken == "") {
+            navigateToOnBoardingScreen()
+        } else {
+            navigateToMainScreen()
+        }
+    }
+
+    private fun navigateToOnBoardingScreen() {
         Intent(this@SplashActivity, OnBoardingActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(this)
+        }
+        finish()
+    }
+
+    private fun navigateToMainScreen() {
+        Intent(this@SplashActivity, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(this)
         }
