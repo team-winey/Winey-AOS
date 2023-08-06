27c8b6b1f5f4e3b3cd7927eaa156d0393111b739
Sangwook123 <operawook@catholic.ac.kr>
2023-08-07 01:52:49 +0900

    [feat] #97 로그인 성공시 메인액티비티 실행

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/OnBoardingActivity.kt
@@ -1,11 +1,16 @@
 package com.android.go.sopt.winey.presentation.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityOnBoardingBinding
+import com.android.go.sopt.winey.presentation.main.MainActivity
 import com.android.go.sopt.winey.util.binding.BindingActivity
+import com.android.go.sopt.winey.util.view.UiState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class OnBoardingActivity :
@@ -15,11 +20,37 @@ class OnBoardingActivity :
         super.onCreate(savedInstanceState)
 
         initKakaoLoginButtonClickListener()
+        loginObserver()
     }
 
-    private fun initKakaoLoginButtonClickListener(){
+    private fun initKakaoLoginButtonClickListener() {
         binding.btnOnboardingKakao.setOnClickListener {
             viewModel.kakaoLogin()
+        }
+    }
+
+    private fun loginObserver() {
+        lifecycleScope.launch {
+            viewModel.loginState.collect { state ->
+
+                when (state) {
+                    is UiState.Loading -> {
+                    }
+
+                    is UiState.Success -> {
+                        val intent = Intent(this@OnBoardingActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    is UiState.Failure -> {
+
+                    }
+
+                    is UiState.Empty -> {
+                    }
+                }
+            }
         }
     }
 }
