47ed1cacbd3e5254079fde2805939e251e086058
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-08 23:08:42 +0900

    [feat] #101 글자수가 맞지 않거나, 공백이나 특수문자를 포함하면 에러 표시

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/nickname/NicknameViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/nickname/NicknameViewModel.kt
@@ -1,16 +1,37 @@
 package com.android.go.sopt.winey.presentation.onboarding.nickname
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.go.sopt.winey.util.code.ErrorCode.CODE_INVALID_LENGTH
+import com.android.go.sopt.winey.util.code.ErrorCode.CODE_SPACE_SPECIAL_CHAR
+import com.android.go.sopt.winey.util.view.InputUiState
+import com.android.go.sopt.winey.util.view.InputUiState.Empty
+import com.android.go.sopt.winey.util.view.InputUiState.Failure
+import com.android.go.sopt.winey.util.view.InputUiState.Success
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class NicknameViewModel : ViewModel() {
-    private val _nickname = MutableLiveData<String>()
-    val nickname: String get() = _nickname.value ?: ""
+    val _nickname = MutableStateFlow("")
+    val nickname: String get() = _nickname.value
 
-//    val isValidNickname: LiveData<Boolean> = _nickname.map { validateNickname(it) }
-//
-//    private fun validateNickname(nickname: String) =
-//        checkLength(nickname) && !containsSpaceOrSpecialChar(nickname)
+    private val _inputUiState = MutableStateFlow<InputUiState>(Empty)
+    val inputUiState: StateFlow<InputUiState> = _nickname.map { checkNickname(it) }
+        .stateIn(
+            initialValue = Empty,
+            scope = viewModelScope,
+            started = WhileSubscribed(PRODUCE_STOP_TIMEOUT)
+        )
+
+    private fun checkNickname(nickname: String): InputUiState {
+        if (nickname.isEmpty()) return Empty
+        if (!checkLength(nickname)) return Failure(CODE_INVALID_LENGTH)
+        if (containsSpaceOrSpecialChar(nickname)) return Failure(CODE_SPACE_SPECIAL_CHAR)
+        return Success
+    }
 
     private fun checkLength(nickname: String) = nickname.length in MIN_LENGTH..MAX_LENGTH
 
@@ -19,7 +40,8 @@ class NicknameViewModel : ViewModel() {
 
     companion object {
         private const val MIN_LENGTH = 1
-        private const val MAX_LENGTH = 8
+        const val MAX_LENGTH = 8
         private const val REGEX_PATTERN = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$"
+        private const val PRODUCE_STOP_TIMEOUT = 5000L
     }
 }
---
+++ app/src/main/java/com/android/go/sopt/winey/util/code/ErrorCode.kt
@@ -0,0 +1,6 @@
+package com.android.go.sopt.winey.util.code
+
+enum class ErrorCode {
+    CODE_INVALID_LENGTH,
+    CODE_SPACE_SPECIAL_CHAR
+}
