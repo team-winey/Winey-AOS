1909c1ccc0e01bcaa20683d11ff3149873835069
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-09 15:10:33 +0900

    [fix] #101 닉네임 에디트텍스트에 포커스가 놓이면 퍼플, 아니면 그레이 색상으로 표시
    - Empty 상태일 때는 selector drawable을 설정하면 되는 거였다.
    - state_focused로 포커스 상태 관찰

--- app/src/main/java/com/android/go/sopt/winey/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/android/go/sopt/winey/util/binding/BindingAdapter.kt
@@ -64,15 +64,10 @@ fun ImageView.setRoundedImage(imageUri: Uri?, drawable: Drawable) {
 
 @BindingAdapter("setBackground")
 fun EditText.setBackground(uiState: InputUiState) {
-    when (uiState) {
-        is Empty -> background = if (this.hasFocus()) {
-            context.drawableOf(R.drawable.shape_purple_line_5_rect)
-        } else {
-            context.drawableOf(R.drawable.shape_gray_line_5_rect)
-        }
-
-        is Success -> background = context.drawableOf(R.drawable.shape_blue_line_5_rect)
-        is Failure -> background = context.drawableOf(R.drawable.shape_red_line_5_rect)
+    background = when (uiState) {
+        is Empty -> context.drawableOf(R.drawable.sel_nickname_edittext_focus_color)
+        is Success -> context.drawableOf(R.drawable.shape_blue_line_5_rect)
+        is Failure -> context.drawableOf(R.drawable.shape_red_line_5_rect)
     }
 }
 
