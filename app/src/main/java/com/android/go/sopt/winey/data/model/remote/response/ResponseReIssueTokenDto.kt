cde7a55fad46fd63cff2e06af640905e4318483e
Sangwook123 <operawook@catholic.ac.kr>
2023-08-09 11:40:49 +0900

    [chore] #97 dto 수정

--- app/src/main/java/com/android/go/sopt/winey/data/model/remote/response/ResponseReIssueTokenDto.kt
+++ app/src/main/java/com/android/go/sopt/winey/data/model/remote/response/ResponseReIssueTokenDto.kt
@@ -1,17 +1,12 @@
 package com.android.go.sopt.winey.data.model.remote.response
 
-import com.android.go.sopt.winey.domain.entity.Login
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseLoginDto(
-    @SerialName("userId")
-    val userId: Int,
+data class ResponseReIssueTokenDto(
     @SerialName("accessToken")
     val accessToken: String,
     @SerialName("refreshToken")
-    val refreshToken: String,
-    @SerialName("isRegistered")
-    val isRegistered: Boolean
+    val refreshToken: String
 )
