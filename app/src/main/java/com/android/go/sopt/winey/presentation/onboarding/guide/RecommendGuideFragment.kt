420acaa9d8dedd9ae43195c1d38e9f4d6abf23db
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-15 00:42:26 +0900

    [feat] #95 앱 사용 가이드 화면에 뷰페이저 적용

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/guide/RecommendGuideFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/guide/RecommendGuideFragment.kt
@@ -2,9 +2,6 @@ package com.android.go.sopt.winey.presentation.onboarding.guide
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentRecommendGuideBinding
 import com.android.go.sopt.winey.util.binding.BindingFragment
@@ -12,19 +9,5 @@ import com.android.go.sopt.winey.util.binding.BindingFragment
 class RecommendGuideFragment : BindingFragment<FragmentRecommendGuideBinding>(R.layout.fragment_recommend_guide) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initNextButtonClickListener()
-    }
-
-    private fun initNextButtonClickListener() {
-        binding.btnGuideNext.setOnClickListener {
-            navigateTo<LevelGuideFragment>()
-        }
-    }
-
-    private inline fun <reified T : Fragment> navigateTo() {
-        parentFragmentManager.commit {
-            replace<T>(R.id.fcv_guide, T::class.simpleName)
-        }
     }
 }
