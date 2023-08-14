bc2a229fa37ee6e18ed00ca6531b69d3c78d17b4
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-14 14:09:10 +0900

    [ui] #95 세이버 스토리 액티비티 구현

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/story/StoryActivity.kt
@@ -0,0 +1,12 @@
+package com.android.go.sopt.winey.presentation.onboarding.story
+
+import android.os.Bundle
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityStoryBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
+
+class StoryActivity : BindingActivity<ActivityStoryBinding>(R.layout.activity_story) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
