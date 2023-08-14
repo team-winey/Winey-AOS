586b0226489c35e032b207cbbe210fb6b68ab8e0
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-14 15:35:48 +0900

    [ui] #95 첫번째 스토리 뷰 디테일 보완

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/story/StoryActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/story/StoryActivity.kt
@@ -1,12 +1,35 @@
 package com.android.go.sopt.winey.presentation.onboarding.story
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityStoryBinding
 import com.android.go.sopt.winey.util.binding.BindingActivity
+import com.android.go.sopt.winey.util.context.colorOf
 
 class StoryActivity : BindingActivity<ActivityStoryBinding>(R.layout.activity_story) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        updateStatusBarColor()
+        setUpDefaultFragment(savedInstanceState)
+    }
+
+    private fun updateStatusBarColor() {
+        window.statusBarColor = colorOf(R.color.gray_50)
+    }
+
+    private fun setUpDefaultFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            navigateTo<FirstStoryFragment>()
+        }
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fcv_story, T::class.simpleName)
+        }
     }
 }
