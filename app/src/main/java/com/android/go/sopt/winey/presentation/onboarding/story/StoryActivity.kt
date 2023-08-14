29c3959cdfa76151a2423df37a6597033eb13751
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-14 16:56:59 +0900

    [feat] #95 화면 전환에 따라 숫자와 텍스트 변경

--- app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/story/StoryActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/onboarding/story/StoryActivity.kt
@@ -1,24 +1,60 @@
 package com.android.go.sopt.winey.presentation.onboarding.story
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityStoryBinding
+import com.android.go.sopt.winey.presentation.onboarding.nickname.NicknameActivity
 import com.android.go.sopt.winey.util.binding.BindingActivity
 import com.android.go.sopt.winey.util.context.colorOf
+import com.android.go.sopt.winey.util.context.stringOf
 
 class StoryActivity : BindingActivity<ActivityStoryBinding>(R.layout.activity_story) {
+    private val viewModel by viewModels<StoryViewModel>()
+    private var currentPageNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
 
         updateStatusBarColor()
+        updateNavigationView(R.string.story_nav_first_detail)
         setUpDefaultFragment(savedInstanceState)
+        initNextButtonClickListener()
+    }
+
+    private fun initNextButtonClickListener() {
+        binding.fabStoryNext.setOnClickListener {
+            when (supportFragmentManager.findFragmentById(R.id.fcv_story)) {
+                is FirstStoryFragment -> {
+                    updateNavigationView(R.string.story_nav_second_detail)
+                    navigateTo<SecondStoryFragment>()
+                }
+
+                is SecondStoryFragment -> {
+                    updateNavigationView(R.string.story_nav_third_detail)
+                    navigateTo<ThirdStoryFragment>()
+                }
+
+                is ThirdStoryFragment -> navigateToNicknameScreen()
+            }
+        }
     }
 
     private fun updateStatusBarColor() {
         window.statusBarColor = colorOf(R.color.gray_50)
+    }
+
+    private fun updateNavigationView(@StringRes resId: Int) {
+        viewModel.apply {
+            updatePageNumber(++currentPageNumber)
+            updateDetailText(stringOf(resId))
+        }
     }
 
     private fun setUpDefaultFragment(savedInstanceState: Bundle?) {
@@ -30,6 +66,13 @@ class StoryActivity : BindingActivity<ActivityStoryBinding>(R.layout.activity_st
     private inline fun <reified T : Fragment> navigateTo() {
         supportFragmentManager.commit {
             replace<T>(R.id.fcv_story, T::class.simpleName)
+        }
+    }
+
+    private fun navigateToNicknameScreen() {
+        Intent(this, NicknameActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(this)
         }
     }
 }
