19845576519ee26306cfe0c2eabdeb117617055f
Sangwook123 <operawook@catholic.ac.kr>
2023-08-15 17:21:33 +0900

    [feat] #113 다이얼로그 코드 작성

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/AlertDialogFragment.kt
@@ -0,0 +1,37 @@
+package com.android.go.sopt.winey.presentation.main
+
+import android.os.Bundle
+import android.view.View
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.FragmentAlertDialogBinding
+import com.android.go.sopt.winey.util.binding.BindingDialogFragment
+
+class AlertDialogFragment(
+    val title: String,
+    val subTitle: String,
+    val negativeButtonLabel: String,
+    val positiveButtonLabel: String,
+    val handleNegativeButton: () -> Unit,
+    val handlePositiveButton: () -> Unit
+) :
+    BindingDialogFragment<FragmentAlertDialogBinding>(R.layout.fragment_alert_dialog) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDialogTitle.text = title
+        binding.tvDialogSub.text = subTitle
+        binding.btnDialogPositive.text = positiveButtonLabel
+        binding.btnDialogNegative.text = negativeButtonLabel
+
+        binding.btnDialogNegative.setOnClickListener {
+            handleNegativeButton.invoke()
+            dismiss()
+        }
+
+        binding.btnDialogPositive.setOnClickListener {
+            handlePositiveButton.invoke()
+            dismiss()
+        }
+    }
+}
