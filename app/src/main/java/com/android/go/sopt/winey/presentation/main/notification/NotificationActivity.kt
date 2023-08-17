d57c90ca0cddde64ef1c80b92b9bcba1f1fa937f
Sangwook123 <operawook@catholic.ac.kr>
2023-08-17 15:33:25 +0900

    [add] #117 액티비티, 뷰모델 생성

---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationActivity.kt
@@ -0,0 +1,15 @@
+package com.android.go.sopt.winey.presentation.main.notification
+
+import android.os.Bundle
+import com.android.go.sopt.winey.R
+import com.android.go.sopt.winey.databinding.ActivityNotificationBinding
+import com.android.go.sopt.winey.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotificationActivity : BindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationViewModel.kt
@@ -0,0 +1,9 @@
+package com.android.go.sopt.winey.presentation.main.notification
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class NotificationViewModel : ViewModel() {
+
+}
