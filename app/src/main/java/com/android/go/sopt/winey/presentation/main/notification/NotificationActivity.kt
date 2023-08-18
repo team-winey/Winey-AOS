94707b65c8caa142c25e695020b9f509cbc1c75c
Sangwook123 <operawook@catholic.ac.kr>
2023-08-18 20:55:10 +0900

    [chore] #117 Notification viewmodel

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationActivity.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationActivity.kt
@@ -1,6 +1,7 @@
 package com.android.go.sopt.winey.presentation.main.notification
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.ActivityNotificationBinding
 import com.android.go.sopt.winey.util.binding.BindingActivity
@@ -9,6 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NotificationActivity : BindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
     private lateinit var notificationAdapter: NotificationAdapter
+    private val viewModel: NotificationViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initNotificationAdapter()
--- app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationViewModel.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/notification/NotificationViewModel.kt
@@ -1,9 +1,5 @@
 package com.android.go.sopt.winey.presentation.main.notification
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
-class NotificationViewModel : ViewModel() {
-
-}
+class NotificationViewModel : ViewModel()
