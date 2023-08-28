4a6ebf92bac02979af0bdb1975ce7a9f00ee33c5
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-28 16:19:59 +0900

    [feat] #147 위니 업로드 이벤트 태깅

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/PhotoFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/feed/upload/PhotoFragment.kt
@@ -8,16 +8,25 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
 import com.android.go.sopt.winey.R
 import com.android.go.sopt.winey.databinding.FragmentPhotoBinding
+import com.android.go.sopt.winey.util.amplitude.AmplitudeUtils
 import com.android.go.sopt.winey.util.binding.BindingFragment
-import com.android.go.sopt.winey.util.context.stringOf
 import com.android.go.sopt.winey.util.fragment.WineyDialogFragment
 import com.android.go.sopt.winey.util.fragment.stringOf
+import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONException
+import org.json.JSONObject
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class PhotoFragment : BindingFragment<FragmentPhotoBinding>(R.layout.fragment_photo) {
     private val uploadViewModel by activityViewModels<UploadViewModel>()
 
+    @Inject
+    lateinit var amplitudeUtils: AmplitudeUtils
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        amplitudeUtils.logEvent("view_upload")
         binding.vm = uploadViewModel
 
         initImageSelectButtonClickListener()
@@ -72,6 +81,7 @@ class PhotoFragment : BindingFragment<FragmentPhotoBinding>(R.layout.fragment_ph
 
     private fun initNextButtonClickListener() {
         binding.btnPhotoNext.setOnClickListener {
+            sendEventToAmplitude()
             navigateToNext()
         }
     }
@@ -87,6 +97,20 @@ class PhotoFragment : BindingFragment<FragmentPhotoBinding>(R.layout.fragment_ph
             replace(R.id.fcv_upload, ContentFragment())
             addToBackStack(null)
         }
+    }
+
+    private fun sendEventToAmplitude() {
+        val eventProperties = JSONObject()
+
+        try {
+            eventProperties.put("button_name", "upload_next_button")
+                .put("paging_number", 2)
+        } catch (e: JSONException) {
+            System.err.println("Invalid JSON")
+            e.printStackTrace()
+        }
+
+        amplitudeUtils.logEvent("click_button", eventProperties)
     }
 
     companion object {
