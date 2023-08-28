e4cfd6958f89c8d0d9faa83e8731bc893b526cef
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-28 16:03:52 +0900

    [feat] #147 추천피드 이벤트 태깅

--- app/src/main/java/com/android/go/sopt/winey/presentation/main/recommend/RecommendFragment.kt
+++ app/src/main/java/com/android/go/sopt/winey/presentation/main/recommend/RecommendFragment.kt
@@ -1,6 +1,7 @@
 package com.android.go.sopt.winey.presentation.main.recommend
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
@@ -13,12 +14,16 @@ import com.android.go.sopt.winey.databinding.FragmentRecommendBinding
 import com.android.go.sopt.winey.presentation.main.MainViewModel
 import com.android.go.sopt.winey.presentation.main.feed.RecommendHeaderAdapter
 import com.android.go.sopt.winey.presentation.main.notification.NotificationActivity
+import com.android.go.sopt.winey.util.amplitude.AmplitudeUtils
 import com.android.go.sopt.winey.util.binding.BindingFragment
 import com.android.go.sopt.winey.util.fragment.snackBar
 import com.android.go.sopt.winey.util.view.UiState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import org.json.JSONException
+import org.json.JSONObject
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecommendFragment : BindingFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
@@ -27,8 +32,12 @@ class RecommendFragment : BindingFragment<FragmentRecommendBinding>(R.layout.fra
     private lateinit var recommendAdapter: RecommendAdapter
     private lateinit var recommendHeaderAdapter: RecommendHeaderAdapter
 
+    @Inject
+    lateinit var amplitudeUtils: AmplitudeUtils
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        amplitudeUtils.logEvent("view_recommend")
 
         initAdapter()
         getRecommendListStateObserver()
@@ -38,10 +47,34 @@ class RecommendFragment : BindingFragment<FragmentRecommendBinding>(R.layout.fra
     }
 
     private fun initAdapter() {
-        recommendAdapter = RecommendAdapter()
+        recommendAdapter = RecommendAdapter(
+            onItemLinkClicked = { id, link ->
+                sendEventToAmplitude(id)
+                displayWebsite(link)
+            }
+        )
         recommendHeaderAdapter = RecommendHeaderAdapter()
         binding.rvRecommendPost.adapter =
             ConcatAdapter(recommendHeaderAdapter, recommendAdapter)
+    }
+
+    private fun sendEventToAmplitude(id: Int) {
+        val eventProperties = JSONObject()
+
+        try {
+            eventProperties.put("contents_id", id)
+                .put("screen_name", "recommend_contents")
+        } catch (e: JSONException) {
+            System.err.println("Invalid JSON")
+            e.printStackTrace()
+        }
+
+        amplitudeUtils.logEvent("click_contents", eventProperties)
+    }
+
+    private fun displayWebsite(link: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        requireActivity().startActivity(intent)
     }
 
     private fun getRecommendListStateObserver() {
