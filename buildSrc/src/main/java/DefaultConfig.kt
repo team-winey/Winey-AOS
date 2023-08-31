7434c433a50c4534e28dc9031c2acda55926c969
leeeha <jxlhe46@seoultech.ac.kr>
2023-08-31 23:26:30 +0900

    [mod] 버전 코드 변경

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -3,6 +3,6 @@ object DefaultConfig {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 10100
+    const val versionCode = 100
     const val versionName = "1.0.0"
 }
