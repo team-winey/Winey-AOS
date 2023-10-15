f6c12bc6fbbea7d0257dabd31ae7cac448accf7d
leeeha <jxlhe46@seoultech.ac.kr>
2023-10-16 00:04:24 +0900

    [feat] #212 절약 기간 내 목표 달성 시 축하 다이얼로그 띄우기

--- app/src/main/java/org/go/sopt/winey/util/binding/BindingDialogFragment.kt
+++ app/src/main/java/org/go/sopt/winey/util/binding/BindingDialogFragment.kt
@@ -14,7 +14,7 @@ abstract class BindingDialogFragment<T : ViewDataBinding>(
     @LayoutRes val layoutRes: Int
 ) : DialogFragment() {
     private var _binding: T? = null
-    protected val binding get() = requireNotNull(_binding) { { "binding object is not initialized" } }
+    protected val binding get() = requireNotNull(_binding) { "binding object is not initialized" }
 
     override fun onStart() {
         super.onStart()
