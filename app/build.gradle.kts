32227e98fccc1b68c60cf665ad24aabca39688de
leeeha <jxlhe46@seoultech.ac.kr>
2023-10-16 14:05:07 +0900

    [fix] 릴리즈 모드에서 앱이 크래시 되는 문제 때문에 코드/리소스 축소 관련 코드 임시 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+//            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
