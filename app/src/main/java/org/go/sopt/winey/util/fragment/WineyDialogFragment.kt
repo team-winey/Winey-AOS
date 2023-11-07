ade5637cf2f3bf3b9ec4435691a54b8c362e525e
Sangwook123 <operawook@catholic.ac.kr>
2023-11-07 17:41:39 +0900

    [refactor] 위니 다이얼로그 생성자 삭제 및 companion object에 newInstance구현

--- app/src/main/java/org/go/sopt/winey/util/fragment/WineyDialogFragment.kt
+++ app/src/main/java/org/go/sopt/winey/util/fragment/WineyDialogFragment.kt
@@ -6,22 +6,26 @@ import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.FragmentAlertDialogBinding
 import org.go.sopt.winey.util.binding.BindingDialogFragment
 
-class WineyDialogFragment(
-    private val title: String,
-    private val subTitle: String,
-    private val negativeButtonLabel: String,
-    private val positiveButtonLabel: String,
-    val handleNegativeButton: () -> Unit,
-    val handlePositiveButton: () -> Unit
-) : BindingDialogFragment<FragmentAlertDialogBinding>(R.layout.fragment_alert_dialog) {
+class WineyDialogFragment :
+    BindingDialogFragment<FragmentAlertDialogBinding>(R.layout.fragment_alert_dialog) {
+    private var handleNegative: (() -> Unit)? = null
+    private var handlePositive: (() -> Unit)? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         dialog?.setCanceledOnTouchOutside(false)
-        initDialogText(title, subTitle, positiveButtonLabel, negativeButtonLabel)
-        initNegativeButtonClickListener(handleNegativeButton)
-        initPositiveButtonClickListener(handlePositiveButton)
+
+        val title = arguments?.getString(ARG_TITLE)
+        val negativeButtonLabel = arguments?.getString(ARG_NEGATIVE_BUTTON_LABEL)
+        val subTitle = arguments?.getString(ARG_SUB_TITLE)
+        val positiveButtonLabel = arguments?.getString(ARG_POSITIVE_BUTTON_LABEL)
+
+        if (title != null && subTitle != null && negativeButtonLabel != null && positiveButtonLabel != null) {
+            initDialogText(title, subTitle, positiveButtonLabel, negativeButtonLabel)
+        }
+        initNegativeButtonClickListener(handleNegative)
+        initPositiveButtonClickListener(handlePositive)
     }
 
     private fun initDialogText(
@@ -36,17 +40,47 @@ class WineyDialogFragment(
         binding.btnDialogNegative.text = negativeButtonLabel
     }
 
-    private fun initNegativeButtonClickListener(handleNegativeButton: () -> Unit) {
+    private fun initNegativeButtonClickListener(handleNegativeButton: (() -> Unit)?) {
         binding.btnDialogNegative.setOnClickListener {
-            handleNegativeButton.invoke()
+            handleNegativeButton?.invoke()
             dismiss()
         }
     }
 
-    private fun initPositiveButtonClickListener(handlePositiveButton: () -> Unit) {
+    private fun initPositiveButtonClickListener(handlePositiveButton: (() -> Unit)?) {
         binding.btnDialogPositive.setOnClickListener {
-            handlePositiveButton.invoke()
+            handlePositiveButton?.invoke()
             dismiss()
+        }
+    }
+
+    companion object {
+        private const val ARG_TITLE = "title"
+        private const val ARG_SUB_TITLE = "subTitle"
+        private const val ARG_NEGATIVE_BUTTON_LABEL = "negativeButtonLabel"
+        private const val ARG_POSITIVE_BUTTON_LABEL = "positiveButtonLabel"
+
+        fun newInstance(
+            title: String,
+            subTitle: String,
+            negativeButtonLabel: String,
+            positiveButtonLabel: String,
+            handleNegativeButton: () -> Unit,
+            handlePositiveButton: () -> Unit
+        ): WineyDialogFragment {
+            val args = Bundle()
+            args.putString(ARG_TITLE, title)
+            args.putString(ARG_SUB_TITLE, subTitle)
+            args.putString(ARG_NEGATIVE_BUTTON_LABEL, negativeButtonLabel)
+            args.putString(ARG_POSITIVE_BUTTON_LABEL, positiveButtonLabel)
+
+            val fragment = WineyDialogFragment()
+            fragment.arguments = args
+
+            fragment.handleNegative = handleNegativeButton
+            fragment.handlePositive = handlePositiveButton
+
+            return fragment
         }
     }
 }
