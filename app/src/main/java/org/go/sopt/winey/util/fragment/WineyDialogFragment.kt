243f60e049f279a5a8119208746f4ca74e4d6bc4
Sangwook123 <operawook@catholic.ac.kr>
2023-11-10 04:49:08 +0900

    [feat] winey dialog fragment 번들과 parcelable 활용하여 객체 전달

--- app/src/main/java/org/go/sopt/winey/util/fragment/WineyDialogFragment.kt
+++ app/src/main/java/org/go/sopt/winey/util/fragment/WineyDialogFragment.kt
@@ -4,84 +4,56 @@ import android.os.Bundle
 import android.view.View
 import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.FragmentAlertDialogBinding
+import org.go.sopt.winey.presentation.model.WineyDialogLabel
 import org.go.sopt.winey.util.binding.BindingDialogFragment
+import org.go.sopt.winey.util.intent.getCompatibleParcelableExtra
 
 class WineyDialogFragment :
     BindingDialogFragment<FragmentAlertDialogBinding>(R.layout.fragment_alert_dialog) {
-    private var handleNegative: (() -> Unit)? = null
-    private var handlePositive: (() -> Unit)? = null
+    private var handleNegative: (() -> Unit) = {}
+    private var handlePositive: (() -> Unit) = {}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         dialog?.setCanceledOnTouchOutside(false)
 
-        val title = arguments?.getString(ARG_TITLE)
-        val negativeButtonLabel = arguments?.getString(ARG_NEGATIVE_BUTTON_LABEL)
-        val subTitle = arguments?.getString(ARG_SUB_TITLE)
-        val positiveButtonLabel = arguments?.getString(ARG_POSITIVE_BUTTON_LABEL)
+        binding.data = arguments?.getCompatibleParcelableExtra(ARG)
 
-        if (title != null && subTitle != null && negativeButtonLabel != null && positiveButtonLabel != null) {
-            initDialogText(title, subTitle, positiveButtonLabel, negativeButtonLabel)
-        }
         initNegativeButtonClickListener(handleNegative)
         initPositiveButtonClickListener(handlePositive)
     }
 
-    private fun initDialogText(
-        title: String,
-        subTitle: String,
-        positiveButtonLabel: String,
-        negativeButtonLabel: String
-    ) {
-        binding.tvDialogTitle.text = title
-        binding.tvDialogSub.text = subTitle
-        binding.btnDialogPositive.text = positiveButtonLabel
-        binding.btnDialogNegative.text = negativeButtonLabel
-    }
-
-    private fun initNegativeButtonClickListener(handleNegativeButton: (() -> Unit)?) {
+    private fun initNegativeButtonClickListener(handleNegativeButton: (() -> Unit)) {
         binding.btnDialogNegative.setOnClickListener {
-            handleNegativeButton?.invoke()
+            handleNegativeButton.invoke()
             dismiss()
         }
     }
 
-    private fun initPositiveButtonClickListener(handlePositiveButton: (() -> Unit)?) {
+    private fun initPositiveButtonClickListener(handlePositiveButton: (() -> Unit)) {
         binding.btnDialogPositive.setOnClickListener {
-            handlePositiveButton?.invoke()
+            handlePositiveButton.invoke()
             dismiss()
         }
     }
 
     companion object {
-        private const val ARG_TITLE = "title"
-        private const val ARG_SUB_TITLE = "subTitle"
-        private const val ARG_NEGATIVE_BUTTON_LABEL = "negativeButtonLabel"
-        private const val ARG_POSITIVE_BUTTON_LABEL = "positiveButtonLabel"
-
+        private const val ARG = "arguments"
         fun newInstance(
-            title: String,
-            subTitle: String,
-            negativeButtonLabel: String,
-            positiveButtonLabel: String,
+            wineyDialogLabel: WineyDialogLabel,
             handleNegativeButton: () -> Unit,
             handlePositiveButton: () -> Unit
         ): WineyDialogFragment {
             val args = Bundle().apply {
-                putString(ARG_TITLE, title)
-                putString(ARG_SUB_TITLE, subTitle)
-                putString(ARG_NEGATIVE_BUTTON_LABEL, negativeButtonLabel)
-                putString(ARG_POSITIVE_BUTTON_LABEL, positiveButtonLabel)
+                putParcelable(ARG, wineyDialogLabel)
             }
 
-            val fragment = WineyDialogFragment()
-            fragment.arguments = args
-
-            fragment.handleNegative = handleNegativeButton
-            fragment.handlePositive = handlePositiveButton
-
-            return fragment
+            return WineyDialogFragment().apply {
+                arguments = args
+                handleNegative = handleNegativeButton
+                handlePositive = handlePositiveButton
+            }
         }
     }
 }
