a40c5758d68bb3304182af4301aab5a8aa75e63f
Sangwook123 <operawook@catholic.ac.kr>
2023-11-20 21:46:23 +0900

    [feat] #226 알림동의 여부 변경 datasource

--- app/src/main/java/org/go/sopt/winey/data/source/AuthDataSource.kt
+++ app/src/main/java/org/go/sopt/winey/data/source/AuthDataSource.kt
@@ -2,12 +2,14 @@ package org.go.sopt.winey.data.source
 
 import org.go.sopt.winey.data.model.remote.request.RequestCreateGoalDto
 import org.go.sopt.winey.data.model.remote.request.RequestLoginDto
+import org.go.sopt.winey.data.model.remote.request.RequestPatchAllowedNotificationDto
 import org.go.sopt.winey.data.model.remote.request.RequestPatchNicknameDto
 import org.go.sopt.winey.data.model.remote.response.ResponseCreateGoalDto
 import org.go.sopt.winey.data.model.remote.response.ResponseGetNicknameDuplicateCheckDto
 import org.go.sopt.winey.data.model.remote.response.ResponseGetUserDto
 import org.go.sopt.winey.data.model.remote.response.ResponseLoginDto
 import org.go.sopt.winey.data.model.remote.response.ResponseLogoutDto
+import org.go.sopt.winey.data.model.remote.response.ResponsePatchAllowedNotificationDto
 import org.go.sopt.winey.data.model.remote.response.ResponseReIssueTokenDto
 import org.go.sopt.winey.data.model.remote.response.base.BaseResponse
 import org.go.sopt.winey.data.service.AuthService
@@ -43,4 +45,9 @@ class AuthDataSource @Inject constructor(
         requestPatchNicknameDto: RequestPatchNicknameDto
     ): BaseResponse<Unit> =
         authService.patchNickname(requestPatchNicknameDto)
+
+    suspend fun patchAllowedNotification(
+        requestPatchAllowedNotificationDto: RequestPatchAllowedNotificationDto
+    ): BaseResponse<ResponsePatchAllowedNotificationDto> =
+        authService.patchAllowedNotification(requestPatchAllowedNotificationDto)
 }
