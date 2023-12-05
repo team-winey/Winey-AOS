173d49f4e59e201d5855d4e518f434022f9f0652
Sangwook123 <operawook@catholic.ac.kr>
2023-12-06 02:01:23 +0900

    [mod] firebase messaging service 수정

--- app/src/main/java/org/go/sopt/winey/configuration/WineyMessagingService.kt
+++ app/src/main/java/org/go/sopt/winey/configuration/WineyMessagingService.kt
@@ -7,22 +7,20 @@ import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.go.sopt.winey.R
 import org.go.sopt.winey.domain.repository.DataStoreRepository
-import org.go.sopt.winey.presentation.main.MainActivity
+import org.go.sopt.winey.presentation.splash.SplashActivity
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class WineyMessagingService : FirebaseMessagingService() {
-
-    private val TAG = "FirebaseService"
 
     @Inject
     lateinit var dataStoreRepository: DataStoreRepository
@@ -30,70 +28,62 @@ class WineyMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
-        Log.i(TAG, token)
-        GlobalScope.launch { dataStoreRepository.saveDeviceToken(token) }
+        CoroutineScope(Dispatchers.IO).launch { dataStoreRepository.saveDeviceToken(token) }
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        Log.d(TAG, "From: " + remoteMessage!!.from)
-        Log.d(TAG,"${remoteMessage.from}")
 
-
-        Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
         if (remoteMessage.data.isNotEmpty()) {
-            Log.i(TAG, remoteMessage.data["body"].toString())
-            Log.i(TAG, remoteMessage.data["title"].toString())
-            Log.d(TAG, "Data Payload: " + remoteMessage.data)
             sendNotification(remoteMessage)
-            Log.d(TAG,remoteMessage.data["feedId"]!!)
-        } else {
-            Log.i(TAG, "data가 비어있습니다. 메시지를 수신하지 못했습니다.")
-            Log.i(TAG, remoteMessage.data.toString())
         }
     }
 
     private fun sendNotification(remoteMessage: RemoteMessage) {
-        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시되도록 함
         val uniId: Int = (System.currentTimeMillis() / 7).toInt()
-        Log.d(TAG,remoteMessage.toString())
-        // 일회용 PendingIntent
-        // PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임한다.
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남긴다. A-B-C-D-B => A-B
+
+        val intent = Intent(this, SplashActivity::class.java)
+        intent.putExtra(KEY_NOTI_TYPE, remoteMessage.data[KEY_NOTI_TYPE])
+        intent.putExtra(KEY_FEED_ID, remoteMessage.data[KEY_FEED_ID])
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
             this,
             uniId,
             intent,
-            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_MUTABLE
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE,
         )
 
-        // 알림 채널 이름
-        val channelId = "channel"
+        val channelId = CHANNEL_ID
 
-        // 알림 소리
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        // 알림에 대한 UI 정보와 작업을 지정한다.
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
-            .setContentTitle(remoteMessage.data["body"].toString()) // 제목
-            .setContentText(remoteMessage.data["title"].toString()) // 메시지 내용
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(remoteMessage.data[KEY_TITLE])
+            .setContentText(remoteMessage.data[KEY_MESSAGE])
             .setAutoCancel(true)
-            .setSound(soundUri) // 알림 소리
-            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // 오레오 버전 이후에는 채널이 필요하다.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel =
-                NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+                NotificationChannel(channelId, NOTICE, NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
-        // 알림 생성
         notificationManager.notify(uniId, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "FirebaseService"
+        private const val KEY_FEED_ID = "feedId"
+        private const val KEY_NOTI_TYPE = "notiType"
+        private const val KEY_TITLE = "title"
+        private const val KEY_MESSAGE = "message"
+        private const val NOTICE = "Notice"
+        private const val CHANNEL_ID = "channel"
     }
 }
