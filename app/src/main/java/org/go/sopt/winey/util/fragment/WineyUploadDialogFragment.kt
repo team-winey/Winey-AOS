c9fc405759832885468aa873db242150244bbfb5
HyeseonBaek <konkukbhs@gmail.com>
2024-01-16 18:49:49 +0900

    [chore] #232 onViewCreated 등록 누락 함수 복구

--- app/src/main/java/org/go/sopt/winey/util/fragment/WineyUploadDialogFragment.kt
+++ app/src/main/java/org/go/sopt/winey/util/fragment/WineyUploadDialogFragment.kt
@@ -14,6 +14,7 @@ class WineyUploadDialogFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addListener()
         setupDialogPosition()
         setupDialogBehavior()
     }
