f3cbc0e4fe166ca2f404a87bd10b8862841dcdc0
leeeha <jxlhe46@gmail.com>
2024-01-22 17:30:32 +0900

    [chore] #233 내부 테스트를 위한 버전 코드 업그레이드

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -3,6 +3,6 @@ object DefaultConfig {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 20
+    const val versionCode = 21
     const val versionName = "1.1.0"
 }
