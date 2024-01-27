96acce673e34575026ccc9ae6f8da0c213283dde
leeeha <jxlhe46@gmail.com>
2024-01-27 20:22:23 +0900

    [chore] #233 appUpdateInfoTask 취소, 실패 테스트를 위한 버전 코드 업그레이드

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -3,6 +3,6 @@ object DefaultConfig {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 22
+    const val versionCode = 23
     const val versionName = "1.1.0"
 }
