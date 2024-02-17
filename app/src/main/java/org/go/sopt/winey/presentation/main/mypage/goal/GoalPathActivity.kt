e3f2e3648a7093ce0239200a9f2005d3001fccc7
leeeha <jxlhe46@gmail.com>
2024-02-17 18:22:18 +0900

    [add] #243 목표 여정 액티비티 추가

---
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathActivity.kt
@@ -0,0 +1,12 @@
+package org.go.sopt.winey.presentation.main.mypage.goal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.go.sopt.winey.R
+
+class GoalPathActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_path)
+    }
+}
