84ebb80682a688ae61e9715f610b15815673e262
leeeha <jxlhe46@gmail.com>
2024-02-20 13:10:52 +0900

    [ui] #243 기사 > 귀족 레벨업 애니메이션 뷰 구현

--- app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel2Fragment.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel2Fragment.kt
@@ -1,7 +1,37 @@
 package org.go.sopt.winey.presentation.main.mypage.goal
 
-import androidx.fragment.app.Fragment
+import android.animation.Animator
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import org.go.sopt.winey.R
+import org.go.sopt.winey.databinding.FragmentGoalPathLevel2Binding
+import org.go.sopt.winey.util.binding.BindingFragment
 
-class GoalPathLevel2Fragment : Fragment() {
+class GoalPathLevel2Fragment: BindingFragment<FragmentGoalPathLevel2Binding>(R.layout.fragment_goal_path_level2) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        initAnimatorListener()
+    }
+
+    private fun initAnimatorListener() {
+        binding.lottieGoalPathStep2.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                binding.ivGoalPathLv2.isVisible = false
+                binding.lottieGoalPathStep2.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                binding.lottieGoalPathStep2.isVisible = false
+                binding.ivGoalPathLv3.isVisible = true
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+        })
+    }
 }
