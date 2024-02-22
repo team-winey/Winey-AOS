0a8605ae4e954d79ee66ce4909bb61fb7246f532
leeeha <jxlhe46@gmail.com>
2024-02-22 14:09:57 +0900

    [feat] #247 현재 유저 레벨에 따라 프래그먼트 전환

--- app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathActivity.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathActivity.kt
@@ -1,39 +1,51 @@
 package org.go.sopt.winey.presentation.main.mypage.goal
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
-import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.ActivityGoalPathBinding
+import org.go.sopt.winey.domain.repository.DataStoreRepository
+import org.go.sopt.winey.presentation.main.mypage.goal.model.UserLevel
 import org.go.sopt.winey.util.binding.BindingActivity
-import org.go.sopt.winey.util.context.snackBar
-import org.go.sopt.winey.util.number.formatAmountNumber
-import org.go.sopt.winey.util.view.UiState
-import java.text.DecimalFormat
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class GoalPathActivity : BindingActivity<ActivityGoalPathBinding>(R.layout.activity_goal_path) {
-    private val viewModel by viewModels<GoalPathViewModel>()
+    @Inject
+    lateinit var dataStoreRepository: DataStoreRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupDefaultFragment(savedInstanceState)
-
-        // todo: 데이터스토어에 저장된 유저 정보로 바인딩 하도록 변경 (굳이 별도의 api 필요 없음)
-        initGetGoalStateObserver()
+        setupGoalPathByUserLevel()
     }
 
-    private fun setupDefaultFragment(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            navigateTo<GoalPathLevel1Fragment>()
+    private fun setupGoalPathByUserLevel() {
+        lifecycleScope.launch {
+            val userInfo = dataStoreRepository.getUserInfo().firstOrNull() ?: return@launch
+            when (userInfo.userLevel) {
+                UserLevel.FIRST.rankName -> {
+                    navigateTo<GoalPathLevel1Fragment>()
+                }
+
+                UserLevel.SECOND.rankName -> {
+                    navigateTo<GoalPathLevel2Fragment>()
+                }
+
+                UserLevel.THIRD.rankName -> {
+                    navigateTo<GoalPathLevel3Fragment>()
+                }
+
+                UserLevel.FORTH.rankName -> {
+                    navigateTo<GoalPathLevel4Fragment>()
+                }
+            }
         }
     }
 
@@ -41,26 +53,5 @@ class GoalPathActivity : BindingActivity<ActivityGoalPathBinding>(R.layout.activ
         supportFragmentManager.commit {
             replace<T>(R.id.fcv_goal_path, T::class.simpleName)
         }
-    }
-
-    private fun initGetGoalStateObserver() {
-        viewModel.getGoalState.flowWithLifecycle(lifecycle).onEach { state ->
-            when (state) {
-                is UiState.Success -> {
-                    state.data?.let { goal ->
-                        binding.tvGoalPathRemainingMoney.text =
-                            getString(R.string.goal_path_remaining_money, goal.remainingMoney.formatAmountNumber())
-                        binding.tvGoalPathRemainingFeed.text =
-                            getString(R.string.goal_path_remaining_feed, goal.remainingFeed)
-                    }
-                }
-
-                is UiState.Failure -> {
-                    snackBar(binding.root) { state.msg }
-                }
-
-                else -> {}
-            }
-        }.launchIn(lifecycleScope)
     }
 }
