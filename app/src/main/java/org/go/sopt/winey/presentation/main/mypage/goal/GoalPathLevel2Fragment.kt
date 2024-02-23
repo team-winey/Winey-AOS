13e23ba7c23dde9df0df3ece72eb44e9bb878efb
leeeha <jxlhe46@gmail.com>
2024-02-23 19:42:29 +0900

    [feat] #247 기사 > 귀족, 귀족 > 황제 레벨업에도 동일 로직 적용

--- app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel2Fragment.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel2Fragment.kt
@@ -4,20 +4,30 @@ import android.animation.Animator
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.flowWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.FragmentGoalPathLevel2Binding
 import org.go.sopt.winey.domain.repository.DataStoreRepository
 import org.go.sopt.winey.util.binding.BindingFragment
 import org.go.sopt.winey.util.fragment.drawableOf
+import org.go.sopt.winey.util.fragment.viewLifeCycle
 import org.go.sopt.winey.util.fragment.viewLifeCycleScope
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class GoalPathLevel2Fragment :
     BindingFragment<FragmentGoalPathLevel2Binding>(R.layout.fragment_goal_path_level2) {
+    private val viewModel by activityViewModels<GoalPathViewModel>()
+
     @Inject
     lateinit var dataStoreRepository: DataStoreRepository
 
@@ -25,6 +35,7 @@ class GoalPathLevel2Fragment :
         super.onViewCreated(view, savedInstanceState)
 
         initGoalPathUnlockGuide()
+        checkLevelUpState()
         initAnimatorListener()
     }
 
@@ -44,6 +55,13 @@ class GoalPathLevel2Fragment :
         }
     }
 
+    private fun checkLevelUpState() {
+        viewModel.levelUpState.flowWithLifecycle(viewLifeCycle).onEach { nowLevelUp ->
+            binding.ivGoalPathLv2.setImageDrawable(drawableOf(R.drawable.img_goal_path_lv2_4))
+            binding.lottieGoalPathStep2.playAnimation()
+        }.launchIn(viewLifeCycleScope)
+    }
+
     private fun initAnimatorListener() {
         binding.lottieGoalPathStep2.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator) {
@@ -58,6 +76,7 @@ class GoalPathLevel2Fragment :
                     lottieGoalPathStep2.isVisible = false
                     ivGoalPathLv3.isVisible = true
                 }
+                navigateTo<GoalPathLevel3Fragment>()
             }
 
             override fun onAnimationCancel(animation: Animator) {
@@ -66,5 +85,11 @@ class GoalPathLevel2Fragment :
             override fun onAnimationRepeat(animation: Animator) {
             }
         })
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        parentFragmentManager.commit {
+            replace<T>(R.id.fcv_goal_path, T::class.simpleName)
+        }
     }
 }
--- app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel3Fragment.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/goal/GoalPathLevel3Fragment.kt
@@ -4,20 +4,30 @@ import android.animation.Animator
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.flowWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.FragmentGoalPathLevel3Binding
 import org.go.sopt.winey.domain.repository.DataStoreRepository
 import org.go.sopt.winey.util.binding.BindingFragment
 import org.go.sopt.winey.util.fragment.drawableOf
+import org.go.sopt.winey.util.fragment.viewLifeCycle
 import org.go.sopt.winey.util.fragment.viewLifeCycleScope
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class GoalPathLevel3Fragment :
     BindingFragment<FragmentGoalPathLevel3Binding>(R.layout.fragment_goal_path_level3) {
+    private val viewModel by activityViewModels<GoalPathViewModel>()
+
     @Inject
     lateinit var dataStoreRepository: DataStoreRepository
 
@@ -25,6 +35,7 @@ class GoalPathLevel3Fragment :
         super.onViewCreated(view, savedInstanceState)
 
         initGoalPathUnlockGuide()
+        checkLevelUpState()
         initAnimatorListener()
     }
 
@@ -44,6 +55,13 @@ class GoalPathLevel3Fragment :
         }
     }
 
+    private fun checkLevelUpState() {
+        viewModel.levelUpState.flowWithLifecycle(viewLifeCycle).onEach { nowLevelUp ->
+            binding.ivGoalPathLv3.setImageDrawable(drawableOf(R.drawable.img_goal_path_lv3_4))
+            binding.lottieGoalPathStep3.playAnimation()
+        }.launchIn(viewLifeCycleScope)
+    }
+
     private fun initAnimatorListener() {
         binding.lottieGoalPathStep3.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator) {
@@ -58,6 +76,7 @@ class GoalPathLevel3Fragment :
                     lottieGoalPathStep3.isVisible = false
                     ivGoalPathLv4.isVisible = true
                 }
+                navigateTo<GoalPathLevel4Fragment>()
             }
 
             override fun onAnimationCancel(animation: Animator) {
@@ -66,5 +85,11 @@ class GoalPathLevel3Fragment :
             override fun onAnimationRepeat(animation: Animator) {
             }
         })
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        parentFragmentManager.commit {
+            replace<T>(R.id.fcv_goal_path, T::class.simpleName)
+        }
     }
 }
