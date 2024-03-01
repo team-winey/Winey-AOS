ad3134ab46498cc51eab36ea9b993360150097a9
leeeha <jxlhe46@gmail.com>
2024-03-01 21:48:53 +0900

    [refactor] #249 뷰홀더에서만 사용되는 변수는 어댑터가 아닌 뷰홀더의 프로퍼티로 정의

--- app/src/main/java/org/go/sopt/winey/presentation/main/feed/WineyFeedGoalAdapter.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/feed/WineyFeedGoalAdapter.kt
@@ -8,6 +8,7 @@ import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.ItemWineyfeedGoalBinding
 import org.go.sopt.winey.domain.entity.UserV2
 import org.go.sopt.winey.presentation.model.UserLevel
+import timber.log.Timber
 import kotlin.math.roundToInt
 
 class WineyFeedGoalAdapter(
@@ -15,9 +16,9 @@ class WineyFeedGoalAdapter(
     private val initialUser: UserV2
 ) : RecyclerView.Adapter<WineyFeedGoalAdapter.WineyFeedGoalViewHolder>() {
     private lateinit var binding: ItemWineyfeedGoalBinding
-    private var isInitialized = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WineyFeedGoalViewHolder {
+        Timber.d("onCreateViewHolder")
         binding = ItemWineyfeedGoalBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -27,17 +28,20 @@ class WineyFeedGoalAdapter(
     }
 
     override fun onBindViewHolder(holder: WineyFeedGoalViewHolder, position: Int) {
+        Timber.d("onBindViewHolder")
         holder.bind()
     }
 
     override fun getItemCount(): Int = ITEM_COUNT
 
-    inner class WineyFeedGoalViewHolder : RecyclerView.ViewHolder(binding.root) {
+    inner class WineyFeedGoalViewHolder: RecyclerView.ViewHolder(binding.root) {
+        private var isInitialized = false
+
         fun bind() {
             if (!isInitialized) {
                 isInitialized = true
 
-                // 최초 1회만 실행 (아이템 뷰의 재활용에 따라 예전 데이터가 다시 바인딩 되는 문제 해결)
+                // 초기 유저 데이터로 최초 1회만 바인딩
                 updateGoalProgressBar(initialUser)
             }
         }
@@ -45,6 +49,7 @@ class WineyFeedGoalAdapter(
 
     fun updateGoalProgressBar(user: UserV2) {
         if (::binding.isInitialized) {
+            // 피드 생성, 삭제에 따라 바뀌는 유저 데이터 반영
             binding.user = user
             updateProgressBarRate(user)
         }
