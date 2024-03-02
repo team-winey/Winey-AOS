f97d002f12083fe4476a188e9df1bf832b745d1f
leeeha <jxlhe46@gmail.com>
2024-03-02 19:23:23 +0900

    [del] #247 사용하지 않는 로그 삭제

--- app/src/main/java/org/go/sopt/winey/presentation/main/feed/WineyFeedGoalAdapter.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/feed/WineyFeedGoalAdapter.kt
@@ -8,6 +8,7 @@ import org.go.sopt.winey.R
 import org.go.sopt.winey.databinding.ItemWineyfeedGoalBinding
 import org.go.sopt.winey.domain.entity.UserV2
 import org.go.sopt.winey.presentation.model.UserLevel
+import timber.log.Timber
 import kotlin.math.roundToInt
 
 class WineyFeedGoalAdapter(
@@ -17,7 +18,6 @@ class WineyFeedGoalAdapter(
     private lateinit var binding: ItemWineyfeedGoalBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WineyFeedGoalViewHolder {
-        Timber.d("onCreateViewHolder")
         binding = ItemWineyfeedGoalBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -27,7 +27,6 @@ class WineyFeedGoalAdapter(
     }
 
     override fun onBindViewHolder(holder: WineyFeedGoalViewHolder, position: Int) {
-        Timber.d("onBindViewHolder")
         holder.bind()
     }
 
