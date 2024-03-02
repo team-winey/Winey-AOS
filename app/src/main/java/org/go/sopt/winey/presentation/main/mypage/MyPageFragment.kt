f8209d2e8efa625fd459c0b9e1bed0917739ac91
leeeha <jxlhe46@gmail.com>
2024-03-03 01:12:06 +0900

    [del] #263 마이페이지 프래그먼트에서 미사용 코드 삭제

--- app/src/main/java/org/go/sopt/winey/presentation/main/mypage/MyPageFragment.kt
+++ app/src/main/java/org/go/sopt/winey/presentation/main/mypage/MyPageFragment.kt
@@ -54,7 +54,6 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
 
     @Inject
     lateinit var amplitudeUtils: AmplitudeUtils
-    private var isNotificationPermissionAllowed = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -286,7 +285,9 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
                     setUpUserDataByGoal(data)
                     animate2weeksSaveGraph(data.amountSavedTwoWeeks)
                     animate2weeksSpendGraph(data.amountSpentTwoWeeks)
+
                     // 마이피드 삭제 후에도 성공 상태 계속 관찰되어 절약금액이 줄어들지 않는 현상 해결하기 위해 state 초기화
+                    // todo: 이 부분 아직 논의 필요함.
                     mainViewModel.initGetUserState()
                 }
 
@@ -482,12 +483,6 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
             }
         }
         textView.text = spannableString
-    }
-
-    private fun showTargetSettingBottomSheet() {
-        val bottomSheet = TargetAmountBottomSheetFragment()
-        bottomSheet.show(parentFragmentManager, bottomSheet.tag)
-        amplitudeUtils.logEvent("view_goalsetting")
     }
 
     companion object {
