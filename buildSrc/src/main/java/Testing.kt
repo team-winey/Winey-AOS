4fc6963957fd2c96b66041d95914fab9c02e99e5
leeeha <jxlhe46@gmail.com>
2024-05-10 22:14:25 +0900

    [fix] #291 junit test 디펜던시 버전 업그레이드

--- buildSrc/src/main/java/Testing.kt
+++ buildSrc/src/main/java/Testing.kt
@@ -1,5 +1,5 @@
 object Testing {
     const val junitVersion = "4.13.2"
     const val espressoVersion = "3.3.0"
-    const val androidTestVersion = "1.1.2"
+    const val androidTestVersion = "1.1.5"
 }
