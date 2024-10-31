d5d021806a655ef577a6cf769bc65bb5f28e98f0
hyeseon <konkukbhs@gmail.com>
2024-10-31 22:41:36 +0900

    [chore] #312 버전코드, 버전네임 업

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -3,6 +3,6 @@ object DefaultConfig {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 32
-    const val versionName = "1.1.0"
+    const val versionCode = 33
+    const val versionName = "1.1.1"
 }
