8b431ff5f3ba66b3b86c9a088bfe4fe3862d41fc
leeeha <jxlhe46@gmail.com>
2025-02-15 17:47:44 +0900

    [chore] #314 버전 코드 업데이트

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -3,6 +3,6 @@ object DefaultConfig {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 33
+    const val versionCode = 34
     const val versionName = "1.1.1"
 }
